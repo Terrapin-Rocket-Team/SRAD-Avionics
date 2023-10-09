daeb4080ec0bef762dc20d0fe9ad6d39dd345bbe
Varun Unnithan <varun.unnithan33@gmail.com>
2023-10-09 19:01:34 -0400

    Updated BMP390 Code

---
+++ Spaceport/23-24/Code/Teensy-Based Avionics/src/BMP390.h
@@ -0,0 +1,35 @@
+#ifndef BMP390_H
+#define BMP390_H
+
+#include <Adafruit_BMP3XX.h>
+#include <Arduino.h>
+#include "Barometer.h"
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+
+class BMP390: public Barometer {
+private:
+    Adafruit_BMP3XX bmp;
+    uint8_t SCK_pin;
+    uint8_t SDA_pin;
+    double groundPressure;
+    double pressure;        // hPa
+    double temp;            // C
+    double altitude;          // m
+
+public:
+    BMP390(uint8_t SCK, uint8_t SDA); 
+    void calibrate(); 
+    double get_pressure();
+    double get_temp();
+    double get_temp_f();
+    double get_pressure_atm();
+    double get_rel_alt_ft();
+    double get_rel_alt_m();
+    String getcsvHeader();
+    String getdataString();
+};
+
+
+#endif //BMP390_H
