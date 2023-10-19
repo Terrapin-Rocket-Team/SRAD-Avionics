7fa4473c2f205cc9edd8991bda8b0e3adacf5d47
Azhanzz <146783694+Azhanzz@users.noreply.github.com>
2023-10-19 03:03:07 -0400

    Update BNO055.h

--- Spaceport/23-24/Code/Teensy-Based Avionics/src/BNO055.h
+++ Spaceport/23-24/Code/Teensy-Based Avionics/src/BNO055.h
@@ -2,4 +2,11 @@
 #include <Arduino.h>
 #include "IMU.h"
 
+virtual void calibrate();
+virtual imu::Quaternion get_orientation();
+virtual imu::Vector<3> get_acceleration();
+virtual imu::Vector<3> get_orientation_euler();
+virtual String getcsvHeader();
+virtual String getdataString();
+
 
