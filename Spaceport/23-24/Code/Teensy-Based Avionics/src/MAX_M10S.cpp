d375bcf5c51503a2848174016e0dd3f50610dd4f
Danny Shi <danny03shi22@gmail.com>
2023-11-07 19:27:16 -0500

    max-m10s updates
    rearranged to suit logic of overall code

--- Spaceport/23-24/Code/Teensy-Based Avionics/src/MAX_M10S.cpp
+++ Spaceport/23-24/Code/Teensy-Based Avionics/src/MAX_M10S.cpp
@@ -4,50 +4,61 @@
 Constructor for Max-M10s 
 */
 MAX_M10S::MAX_M10S() {
-    orgin.x() = m10s.getLatitude();
-    orgin.y() = m10s.getLongitude();
-    orgin.z() = m10s.getAltitude() * 1000;
-    read_gps();
+
 }
+
+//need to update origin some how
 
 void MAX_M10S::calibrate() {
     Serial.println("Max-M10s");
 }
 
+/*
+used to update all instance variables 
+*/
 void MAX_M10S::read_gps() {
-    get_velocity();
-    get_alt();
-    get_pos();
-    get_displace();
-    get_gps_time();
-    get_fix_qual();
+    altitude = m10s.getAltitude();
+
+    pos.x() = m10s.getLatitude();
+    pos.y() = m10s.getLongitude();
+
+    velocity.x() = ((pos.x() * 111139) - displacement.x()) / (millis() * 1000 - gps_time);
+    velocity.y() = ((pos.y() * 111139) - displacement.y()) / (millis() * 1000 - gps_time);
+    velocity.z() = ((altitude * 1000) - displacement.z()) / (millis() * 1000 - gps_time); 
+
+    displacement.x() = (m10s.getLatitude() - orgin.x()) * 111139.0;
+    displacement.y() = (m10s.getLongitude() - orgin.y()) * 111139.0;
+    displacement.z() = ((m10s.getAltitude() * 1000) - orgin.z());
+
+    gps_time = (millis() * 1000);
+
+    real_time.x() = m10s.getHour();
+    real_time.y() = m10s.getMinute();
+    real_time.z() = m10s.getSecond();
+
+    fix_qual = m10s.getSIV();
 }
 
 /*
 return altitude in mm
 */
 double MAX_M10S::get_alt() {
-    return altitude = m10s.getAltitude();
-}
-
-/*
-return the velocity (meters per second)
-there probably issues with floating points
-*/
-imu::Vector<3> MAX_M10S::get_velocity() {
-    velocity.x() = ((m10s.getLatitude() * 111139.0) - displacement.x()) / (millis() - gps_time);
-    velocity.y() = ((m10s.getLongitude() * 111139.0) - displacement.y()) / (millis() - gps_time);
-    velocity.z() = ((m10s.getAltitude() * 1000.0) - displacement.z()) / (millis() - gps_time);
-    return velocity;
+    return altitude;
 }
 
 /*
 returns the lat and long of the rocket to the 7th sig fig 
 */
 imu::Vector<2> MAX_M10S::get_pos() {
-    pos.x() = m10s.getLatitude();
-    pos.y() = m10s.getLongitude();
     return pos;
+} 
+
+/*
+return the velocity (meters per second)
+there probably issues with floating points
+*/
+imu::Vector<3> MAX_M10S::get_velocity() {
+    return velocity;
 }
 
 /*
@@ -55,9 +66,6 @@ retern the displacement since the origin
 there is probably issues with floating point arithmetic 
 */
 imu::Vector<3> MAX_M10S::get_displace() {
-    displacement.x() = (m10s.getLatitude() - orgin.x()) * 111139.0;
-    displacement.y() = (m10s.getLongitude() - orgin.y()) * 111139.0;
-    displacement.z() = ((m10s.getAltitude() * 1000) - orgin.z());
     return displacement;
 }
 
@@ -65,21 +73,31 @@ imu::Vector<3> MAX_M10S::get_displace() {
 time since in initialization in seconds
 */
 double MAX_M10S::get_gps_time() {
-    return gps_time = (millis() * 1000);
+    return gps_time ;
 }
+
 /*
 returns the current hour, min, and sec 
 */
-imu::Vector<3> MAX_M10S::get_time() {
-    time.x() = m10s.getHour();
-    time.y() = m10s.getMinute();
-    time.z() = m10s.getSecond();
-    return time;
+imu::Vector<3> MAX_M10S::get_time() {   
+    return real_time;
 }
 
 /*
 return the number of satellites to indicate quality of data
 */
 int MAX_M10S::get_fix_qual() {
-    return fix_qual = m10s.getSIV();
+    return fix_qual;
 }   
+
+String getcsvHeader() {
+    
+
+}
+
+String getdataString() {
+
+
+}
+
+// Danny S.
