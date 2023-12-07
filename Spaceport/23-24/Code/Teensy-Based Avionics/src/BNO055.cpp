0d5b60edc2c865c55372ca4c1ac1e78a7cdc1253
Azhanzz <146783694+Azhanzz@users.noreply.github.com>
2023-12-07 16:57:08 -0500

    Update BNO055.cpp

--- Spaceport/23-24/Code/Teensy-Based Avionics/src/BNO055.cpp
+++ Spaceport/23-24/Code/Teensy-Based Avionics/src/BNO055.cpp
@@ -4,6 +4,30 @@ include "BNO055.h"
 BNO055::BNO055() {
 }
 
+void setupbno()
+{
+  if (!bno.begin())
+  {
+    while (1);
+  }
+  bno.setExtCrystalUse(true);
+}
+
+void calibrate_bno()
+{
+  uint8_t system, gyro, accel, mag, i = 0;
+  while ((system != 3) || (gyro != 3) || (accel != 3) || (mag != 3))
+  {
+    bno.getCalibration(&system, &gyro, &accel, &mag);
+    i = i + 1;
+    if (i == 10)
+    { 
+      i = 0;
+    }
+    delay(10);
+  }
+}
+
 
 virtual imu::Quaternion get_orientation() {
 return getQuat();
@@ -25,7 +49,7 @@ return String(accleration_vec.x) +”,” + String(accleration_vec.y) +”,” +
 }  
 
 virtual String getcsvHeader() {
-String s = “acceleration_X (m/s/s), acceleration_Y (m/s/s), acceleration_Z (m/s/s) **** euler (orientation x), euler (orientation y), euler (orientation z)”
+String s = “acceleration_X (m/s/s), acceleration_Y (m/s/s), acceleration_Z (m/s/s), euler (orientation x), euler (orientation y), euler (orientation z)”;
 
 
 
