606d0f5a3b2fd3c4896288ce8d3ef301203ef173
jhauerst <114038876+jhauerst@users.noreply.github.com>
2024-01-09 23:09:01 -0500

    completed initial radio code

--- Spaceport/23-24/Code/Teensy-Based Avionics/include/Radio.h
+++ Spaceport/23-24/Code/Teensy-Based Avionics/include/Radio.h
@@ -3,6 +3,12 @@
 
 #include <Arduino.h>
 
+/*
+Types:
+0 - Telemetry
+1 - Video
+*/
+
 class Radio
 {
 public:
@@ -12,6 +18,18 @@ public:
     virtual String rx() = 0;
     virtual bool encode(String &message, int type) = 0;
     virtual bool decode(String &message, int type) = 0;
+    virtual bool send(String message, int type) = 0;
+    virtual String receive(int type) = 0;
+    virtual int RSSI() = 0;
+};
+
+struct APRSConfig
+{
+    char *CALLSIGN;
+    char *TOCALL;
+    char *PATH;
+    char *SYMBOL;
+    char *OVERLAY;
 };
 
 #endif // RADIO_H
