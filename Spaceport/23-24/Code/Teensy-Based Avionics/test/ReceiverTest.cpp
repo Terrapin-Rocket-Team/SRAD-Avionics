a28095b90a2cdfd97d0511997315f44ef877184c
Danny Shi <dshi0322@terpmail.umd.edu>
2024-01-23 17:18:25 -0500

    merged and fixed code

--- Spaceport/23-24/Code/Teensy-Based Avionics/test/ReceiverTest.cpp
+++ Spaceport/23-24/Code/Teensy-Based Avionics/test/ReceiverTest.cpp
@@ -4,27 +4,22 @@
 #define CALLSIGN "KC3UTM"
 #define TOCALL "APRS"
 #define PATH "WIDE1-1"
-#define SYMBOL "["
-#define OVERLAY "/"
 
-APRSConfig confi = {CALLSIGN, TOCALL, PATH, SYMBOL, OVERLAY};
+APRSConfig confi = {CALLSIGN, TOCALL, PATH, '[', '/'};
 RFM69HCW receive = {433775000, false, false, confi};
 
 void setup() {
     Serial.begin(9600);
     while (!Serial) {
-        Serial.println("receiver test");
+        Serial.println("receiver loading");
     }
-    SPIClass* spi;
-    receive.begin(spi, 3, 4, 433);
+    SPIClass* sp = &SPI;
+    receive.begin(sp, 3, 4);
     Serial.println("RFM69r began");
 }
 
 void loop() {
-    delay(1000);
     if (receive.available()) {
-            Serial.println(receive.receive(0));
-    } else {
-        Serial.println("not available");
-    }
+        Serial.println(receive.receive(ENCT_TELEMETRY));
+    } 
 }
