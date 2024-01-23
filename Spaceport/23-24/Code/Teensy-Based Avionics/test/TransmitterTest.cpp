22f10ef247ef96755359d566ce40ebd86d0a1972
Danny Shi <dshi0322@terpmail.umd.edu>
2024-01-23 17:18:25 -0500

    merged and fixed code

--- Spaceport/23-24/Code/Teensy-Based Avionics/test/TransmitterTest.cpp
+++ Spaceport/23-24/Code/Teensy-Based Avionics/test/TransmitterTest.cpp
@@ -4,10 +4,8 @@
 #define CALLSIGN "KC3UTM"
 #define TOCALL "APRS"
 #define PATH "WIDE1-1"
-#define SYMBOL "["
-#define OVERLAY "/"
 
-APRSConfig confi = {CALLSIGN, TOCALL, PATH, SYMBOL, OVERLAY};
+APRSConfig confi = {CALLSIGN, TOCALL, PATH, '[', '/'};
 RFM69HCW transmit = {433775000, true, false, confi};
 
 void setup() {
@@ -15,12 +13,12 @@ void setup() {
     while (!Serial) {
         Serial.println("transmitter test");
     }
-    SPIClass* spi;
-    transmit.begin(spi, 1, 2, 433);
+    SPIClass *sp = &SPI;
+    transmit.begin(sp, 1, 2);
     Serial.println("RFM69t began");
 }
 
 void loop() {
-    delay(1000);
-    transmit.send("sending a message", 0);
+    delay(500);
+    transmit.send("39.336896667,-77.337067833,480,0,31,H,0,11:40:51", ENCT_TELEMETRY);
 }
