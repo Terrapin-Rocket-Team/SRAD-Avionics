49d872b76acb7e5d6e17f945c841897e2f5a065f
jhauerst <114038876+jhauerst@users.noreply.github.com>
2024-02-18 23:53:04 -0500

    small update to radio test files

---
+++ Spaceport/23-24/Code/Teensy-Based Avionics/test/ReceiverTest.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include "RFM69HCW.h"
+
+#define CALLSIGN "KC3UTM"
+#define TOCALL "APRS"
+#define PATH "WIDE1-1"
+
+APRSConfig confi = {CALLSIGN, TOCALL, PATH, '[', '/'};
+RFM69HCW receive = {433775000, false, false, confi};
+
+void setup()
+{
+    Serial.begin(9600);
+    while (!Serial)
+    {
+        Serial.println("receiver loading");
+    }
+    SPIClass *sp = &SPI;
+    receive.begin(sp, 3, 4, 5);
+    Serial.println("RFM69r began");
+}
+
+void loop()
+{
+    if (receive.available())
+    {
+        Serial.println(receive.receive(ENCT_TELEMETRY));
+    }
+}
