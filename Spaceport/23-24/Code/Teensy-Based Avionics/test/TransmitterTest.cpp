cfb64ac5e03b59d267d668ae5c078c253f8c4514
jhauerst <114038876+jhauerst@users.noreply.github.com>
2024-02-26 21:36:13 -0500

    final fixes, cleaning up, and updating tests

--- Spaceport/23-24/Code/Teensy-Based Avionics/test/TransmitterTest.cpp
+++ Spaceport/23-24/Code/Teensy-Based Avionics/test/TransmitterTest.cpp
@@ -5,24 +5,24 @@
 #define TOCALL "APRS"
 #define PATH "WIDE1-1"
 
-APRSConfig confi = {CALLSIGN, TOCALL, PATH, '[', '/'};
-RFM69HCW transmit = {433775000, true, false, confi};
+APRSConfig config = {CALLSIGN, TOCALL, PATH, '[', '/'};
+RadioSettings settings = {433.775, true, false, &hardware_spi, 10, 2, 9};
+RFM69HCW transmit = {settings, config};
 
 void setup()
 {
     Serial.begin(9600);
     while (!Serial)
-    {
-        Serial.println("transmitter test");
-    }
-    SPIClass *sp = &SPI;
-    transmit.begin(sp, 1, 2, 6);
+        ;
+
+    if (!transmit.begin())
+        Serial.println("Transmitter failed to begin");
+
     Serial.println("RFM69t began");
 }
 
 void loop()
 {
-    delay(500);
+    delay(1000);
     transmit.send("39.336896667,-77.337067833,480,0,31,H,0,11:40:51", ENCT_TELEMETRY);
-    
 }
