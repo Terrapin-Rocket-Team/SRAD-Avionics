ef969c74aff30f2b28a78d74c61ca7b019daae50
Abhi Senthilkumar <asenthil@terpmail.umd.edu>
2024-04-05 19:29:07 -0400

    wrote all the code (in theory) to run the kalman filter

--- Spaceport/23-24/Code/Teensy-Based Avionics/src/AprilFilter.cpp
+++ Spaceport/23-24/Code/Teensy-Based Avionics/src/AprilFilter.cpp
@@ -26,3 +26,36 @@ Matrix get_H(int has_gps, int has_barometer){
       0, 0, 1.0 / sum * has_barometer, 0, 0, 0};
   return Matrix(3, 6, result);
 }
+
+LinearKalmanFilter initializeFilter(){
+  double* x = new double[6] {0, 0, 0, 0, 0, 0};
+  Matrix X = new Matrix(6, 1, x);
+  double* u = new double[3] {0, 0, 0};
+  Matrix U = new Matrix(3, 1, u);
+  double *p = new double[36]{1, 0, 0, 1, 0, 0,
+      0, 1, 0, 0, 1, 0,
+      0, 0, 1, 0, 0, 1,
+      1, 0, 0, 1, 0, 0,
+      0, 1, 0, 0, 1, 0,
+      0, 0, 1, 0, 0, 1};
+  Matrix P = new Matrix(6, 6, p);
+  double *r = new double[16] {1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 0, 0, 1};
+  Matrix R = new Matrix(4, 4, r);
+  
+  return new LinearKalmanFilter(X, U, P, get_F(0), get_G(0), R);
+}
+
+Matrix iterateFilter(LinearKalmanFilter kf, int dt, double* input, double* measurement, int has_gps, int has_barometer){
+  Matrix meas = Matrix(4, 1, measurement);
+  Matrix inp = Matrix(3, 1, input);
+  Matrix state = kf.iterate(meas, inp, get_F(dt), get_G(dt), get_H(has_gps, has_barometer));
+  double* ret = new double[6];
+  double* st = state.getArr();
+  for(int i = 0; i < 6; ++i){
+    ret[i] = st[i];
+  }
+  return ret;
+}
