418449ead77d62651f40fd242dd7f0031c30dfd3
Abhi Senthilkumar <asenthil@terpmail.umd.edu>
2024-04-15 01:07:28 -0400

    tried to fix memory issues based on avi's suggestions

--- Spaceport/23-24/Code/Teensy-Based Avionics/src/AprilFilter.cpp
+++ Spaceport/23-24/Code/Teensy-Based Avionics/src/AprilFilter.cpp
@@ -31,23 +31,23 @@ Matrix get_H(int has_gps, int has_barometer){
 
 LinearKalmanFilter *initializeFilter(){
   double* x = new double[6] {0, 0, 0, 0, 0, 0};
-  Matrix* X = new Matrix(6, 1, x);
+  Matrix* X = Matrix(6, 1, x);
   double* u = new double[3] {0, 0, 0};
-  Matrix* U = new Matrix(3, 1, u);
+  Matrix* U = Matrix(3, 1, u);
   double *p = new double[36]{1, 0, 0, 1, 0, 0,
       0, 1, 0, 0, 1, 0,
       0, 0, 1, 0, 0, 1,
       1, 0, 0, 1, 0, 0,
       0, 1, 0, 0, 1, 0,
       0, 0, 1, 0, 0, 1};
-  Matrix* P = new Matrix(6, 6, p);
+  Matrix* P = Matrix(6, 6, p);
   double *r = new double[16] {1, 0, 0, 0,
       0, 1, 0, 0,
       0, 0, 1, 0,
       0, 0, 0, 1};
-  Matrix* R = new Matrix(4, 4, r);
+  Matrix* R = Matrix(4, 4, r);
   
-  return new LinearKalmanFilter(*X, *U, *P, get_F(0), get_G(0), *R);
+  return LinearKalmanFilter(X, U, P, get_F(0.1), get_G(0.1), R);
 }
 
 double* iterateFilter(LinearKalmanFilter kf, int dt, double* input, double* measurement, int has_gps, int has_barometer){
