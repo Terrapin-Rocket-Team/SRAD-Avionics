610ada1a16b9619111b001efb1bf7792632a559b
Varun Unnithan <varun.unnithan33@gmail.com>
2024-05-11 17:47:18 -0400

    change?

--- Spaceport/23-24/Code/Teensy-Based Avionics/src/main.cpp
+++ Spaceport/23-24/Code/Teensy-Based Avionics/src/main.cpp
@@ -1,133 +1,32 @@
 #include <Arduino.h>
-#include "State.h"
-#include "BMP390.h"
-#include "BNO055.h"
-#include "MAX_M10S.h"
-#include "DS3231.h"
 #include "RFM69HCW.h"
-#include "RecordData.h"
 
-BNO055 bno(13, 12);         // I2C Address 0x29
-BMP390 bmp(13, 12);         // I2C Address 0x77
-MAX_M10S gps(13, 12, 0x42); // I2C Address 0x42
-DS3231 rtc();               // I2C Address 0x68
-APRSConfig config = {"KC3UTM", "APRS", "WIDE1-1", '[', '/'};
-RadioSettings settings = {915.0, true, false, &hardware_spi, 10, 31, 32};
-RFM69HCW radio = {&settings, &config};
-State computer;// = useKalmanFilter = true, stateRecordsOwnData = true
-uint32_t radioTimer = millis();
+#define CALLSIGN "KC3UTM"
+#define TOCALL "APRS"
+#define PATH "WIDE1-1"
 
-PSRAM *ram;
-
-#define BUZZER 33
-#define BMP_ADDR_PIN 36
-
-static double last = 0; // for better timing than "delay(100)"
+APRSConfig config = {CALLSIGN, TOCALL, PATH, '[', '/'};
+RadioSettings settings = {915.0, false, false, &hardware_spi, 10, 31, 32};
+RFM69HCW receive = {&settings, &config};
 
 void setup()
 {
-    recordLogData(INFO, "Initializing Avionics System. 10 second delay to prevent unnecessary file generation.", TO_USB);
-    delay(5000);
+    Serial.begin(9600);
+    // while (!Serial)
+    //     ;
 
-    pinMode(BMP_ADDR_PIN, OUTPUT);
-    digitalWrite(BMP_ADDR_PIN, HIGH);
+    if (!receive.begin())
+        Serial.println("Reciever failed to begin");
 
-    pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(BUZZER, OUTPUT); //its very loud during testing
-
-    pinMode(0, OUTPUT); // RASPBERRY PI TURN ON
-    pinMode(1, OUTPUT); // RASPBERRY PI TURN ON
-
-
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
-    digitalWrite(LED_BUILTIN, LOW);
-    ram = new PSRAM(); // init after the SD card for better data logging.
-
-    // The SD card MUST be initialized first to allow proper data logging.
-    if (setupSDCard())
-    {
-
-        recordLogData(INFO, "SD Card Initialized");
-        digitalWrite(BUZZER, HIGH);
-        delay(1000);
-        digitalWrite(BUZZER, LOW);
-    }
-    else
-    {
-        recordLogData(ERROR, "SD Card Failed to Initialize");
-
-        digitalWrite(BUZZER, HIGH);
-        delay(200);
-        digitalWrite(BUZZER, LOW);
-        delay(200);
-        digitalWrite(BUZZER, HIGH);
-        delay(200);
-        digitalWrite(BUZZER, LOW);
-    }
-
-    // The PSRAM must be initialized before the sensors to allow for proper data logging.
-
-    if (ram->init())
-        recordLogData(INFO, "PSRAM Initialized");
-    else
-        recordLogData(ERROR, "PSRAM Failed to Initialize");
-
-    if (!computer.addSensor(&bmp))
-        recordLogData(INFO, "Failed to add BMP390 Sensor");
-    if (!computer.addSensor(&gps))
-        recordLogData(INFO, "Failed to add MAX_M10S Sensor");
-    if (!computer.addSensor(&bno))
-        recordLogData(INFO, "Failed to add BNO055 Sensor");
-    computer.setRadio(&radio);
-    if (computer.init()){
-        recordLogData(INFO, "All Sensors Initialized");
-        digitalWrite(BUZZER, HIGH);
-        delay(1000);
-        digitalWrite(BUZZER, LOW);
-    }
-    else{
-        recordLogData(ERROR, "Some Sensors Failed to Initialize. Disabling those sensors.");
-        digitalWrite(BUZZER, HIGH);
-        delay(200);
-        digitalWrite(BUZZER, LOW);
-        delay(200);
-        digitalWrite(BUZZER, HIGH);
-        delay(200);
-        digitalWrite(BUZZER, LOW);
-    }
-
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    sendSDCardHeader(computer.getCsvHeader());
+    Serial.println("RFM69r began");
 }
-static bool more = false;
+
 void loop()
 {
-    double time = millis();
-
-    if (time - radioTimer >= 2000)
+    if (receive.available())
     {
-        more = computer.transmit();
-        radioTimer = time;
-    }
-    if (radio.mode() != RHGenericDriver::RHModeTx && more){
-        more = !radio.sendBuffer();
-    }
-    if (time - last < 100)
-        return;
-
-    last = time;
-    computer.updateState();
-    recordLogData(INFO, computer.getStateString(), TO_USB);
-
-
-    // RASPBERRY PI TURN ON
-    if (time / 1000.0 > 600) {
-        digitalWrite(0, HIGH);
-    }
-    if (computer.getStageNum() == 1) {
-        digitalWrite(1, HIGH);
+        Serial.println("s");
+        Serial.println(receive.receive(ENCT_GROUNDSTATION));
+        Serial.println("e");
     }
 }
