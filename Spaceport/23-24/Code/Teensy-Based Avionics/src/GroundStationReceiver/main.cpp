e64af2d485e0ffa03fbcc120d1772b0bf8a7e5fd
Drew Brandt <ahbrandt@umd.edu>
2024-05-16 01:07:53 -0400

    adjusted lib deps and added encoded serial ouput

--- Spaceport/23-24/Code/Teensy-Based Avionics/src/GroundStationReceiver/main.cpp
+++ Spaceport/23-24/Code/Teensy-Based Avionics/src/GroundStationReceiver/main.cpp
@@ -1,4 +1,6 @@
 #include "RFM69HCW.h"
+#include "EncodeAPRSForSerial.h"
+#include "Adafruit_BNO055.h"
 #include <Arduino.h>
 
 APRSHeader header;
@@ -8,23 +10,21 @@ RFM69HCW radio(&settings);
 void setup()
 {
     delay(2000);
-    if(!radio.init())
+    if (!radio.init())
         Serial.println("Radio failed to initialize");
     else
         Serial.println("Radio initialized");
-
-    
 }
 
 void loop()
 {
-    if(radio.update())
+    if (radio.update())
     {
-        if(radio.dequeueReceive(&msg))
+        if (radio.dequeueReceive(&msg))
         {
-            printf("Received message (%d):", radio.RSSI());
-            printf("\nHeader: %s>%s,%s:\n", msg.header.CALLSIGN, msg.header.TOCALL, msg.header.PATH);
-            printf("Data: %f, %f, %f, %f, %f, %d, %f, %f, %f\n", msg.data.lat, msg.data.lng, msg.data.alt, msg.data.spd, msg.data.hdg, msg.data.stage, msg.data.orientation.x(), msg.data.orientation.y(), msg.data.orientation.z());
+            char buffer[255];
+            aprsToSerial::encodeAPRSForSerial(msg, buffer, 255, radio.RSSI());
+            Serial.println(buffer);
         }
     }
 }
