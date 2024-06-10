d18591f2b269fe60edf5ac5212258ebbe876832b
Varun Unnithan <varun.unnithan33@gmail.com>
2024-06-10 07:01:08 -0400

    small chnges to mux utils

--- Side_Projects/Live Video/mux/3-stream/faker.py
+++ Side_Projects/Live Video/mux/3-stream/faker.py
@@ -1,6 +1,5 @@
 video1 = "./inputs/vid1.av1"
 video2 = "./inputs/vid2.av1"
-source3 = "./inputs/whattaburger.txt"
 
 output_dest = "./output/mux.bin"
 
@@ -15,7 +14,7 @@ telemetry_string = "Source:KC3TM,Destination:321,Path:555,Type:Position Without 
 # The same is done for source 2. This is repeated 15 times, alternating between the two sources. Then, the number 3 is written, 
 # followed by the size of the constant string, followed by the constant string. This pattern is repeated until the end of the file.
 def write_mux():
-    with open(source3, "rb") as f1, open(video2, "rb") as f2, open(output_dest, "wb") as out:
+    with open(video1, "rb") as f1, open(video2, "rb") as f2, open(output_dest, "wb") as out:
         # repeat while there are still bytes to read from the files
         escape = False
         while not escape:
@@ -35,7 +34,7 @@ def write_mux():
                 out.write(b'\x02')
                 out.write(source2_size.to_bytes(2, byteorder='big'))
                 out.write(source2)
-            out.write(b'\x03')
+            out.write(b'\xfe')
             out.write(len(telemetry_string).to_bytes(2, byteorder='big'))
             out.write(telemetry_string.encode('utf-8'))
 
--- Side_Projects/Live Video/mux/SD-to-Serial/src/main.cpp
+++ Side_Projects/Live Video/mux/SD-to-Serial/src/main.cpp
@@ -24,10 +24,16 @@ void setup() {
         return;
     }
 
+    delay(5000);
+
     // Read from the file until there's nothing else in it
     while (file.available()) {
         // Read a byte from the file and send it to the Serial buffer
         Serial.write(file.read());
+
+        // Hollistically, above lines take up 6% of desired time
+        // Means every second, we manually delay .94 seconds, assuming bitrate of 600kbps (15575)
+        delayNanoseconds(25000); 
     }
 
     // Close the file
