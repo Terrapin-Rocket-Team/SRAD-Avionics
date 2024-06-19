ef8eeb8584bf39b5d7b2eb19ecd1050b96948e3c
Drew Brandt <ahbrandt@umd.edu>
2024-06-19 01:31:04 -0600

    completely fixed 100 percent
    except not haha gotcha

--- Spaceport/23-24/Code/Teensy-Based Avionics/src/Radio/RadioHandler.h
+++ Spaceport/23-24/Code/Teensy-Based Avionics/src/Radio/RadioHandler.h
@@ -14,40 +14,53 @@ namespace radioHandler
     {
         bb.aonoff(BUZZER, 100, 2);
         char log[100];
-        if (cmd.data.Launch != old.Launch && cmd.data.Launch >= 0)
+        if (cmd.data.Launch > 0)
         {
             snprintf(log, 100, "Launch Command Changed to %d.", cmd.data.Launch);
             recordLogData(INFO, log);
             currentCmdData.Launch = cmd.data.Launch;
         }
-        if (cmd.data.MinutesUntilPowerOn != old.MinutesUntilPowerOn && cmd.data.MinutesUntilPowerOn >= 0)
+        if ( cmd.data.MinutesUntilPowerOn >= 0)
         {
             snprintf(log, 100, "Power On Time Changed: %d with %d minutes remaining.", cmd.data.MinutesUntilPowerOn, (int)(currentCmdData.MinutesUntilPowerOn - time) / 60000);
             recordLogData(INFO, log);
             currentCmdData.MinutesUntilPowerOn = cmd.data.MinutesUntilPowerOn * 60 * 1000 + time;
         }
-        if (cmd.data.MinutesUntilVideoStart != old.MinutesUntilVideoStart && cmd.data.MinutesUntilVideoStart >= 0)
+        if (cmd.data.MinutesUntilVideoStart >= 0)
         {
             snprintf(log, 100, "Video Start Time Changed: %d with %d minutes remaining.", cmd.data.MinutesUntilVideoStart, (int)(currentCmdData.MinutesUntilVideoStart - time) / 60000);
             recordLogData(INFO, log);
             currentCmdData.MinutesUntilVideoStart = cmd.data.MinutesUntilVideoStart * 60 * 1000 + time;
         }
-        if (cmd.data.MinutesUntilDataRecording != old.MinutesUntilDataRecording && cmd.data.MinutesUntilDataRecording >= 0)
+        if (cmd.data.MinutesUntilDataRecording >= 0)
         {
             snprintf(log, 100, "Data Recording Time Changed: %d with %d minutes remaining.", cmd.data.MinutesUntilDataRecording, (int)(currentCmdData.MinutesUntilDataRecording - time) / 60000);
             recordLogData(INFO, log);
             currentCmdData.MinutesUntilDataRecording = cmd.data.MinutesUntilDataRecording * 60 * 1000 + time;
         }
+        // if(currentCmdData.Reset)
+        // {
+        //     recordLogData(INFO, "Reset Command Received. Restarting Flight Computer.");
+        //     currentCmdData.Reset = true;
+        // }
     }
 
     void processCurrentCmdData(APRSCmdData &currentCmdData, State &computer, Pi &rpi, int time)
     {
+        // if(currentCmdData.Reset)
+        // {
+        //     computer.init();
+        //     computer.launch();
+        //     rpi.setOn(true);
+        //     rpi.setRecording(true);
+        //     computer.setRecordOwnFlightData(true);
+        //     currentCmdData.Reset = false;
+        // }
         if (currentCmdData.Launch && computer.getStageNum() == 0)
         {
             recordLogData(INFO, "Launch Command Received. Launching Rocket.");
             computer.launch();
         }
-
         if (time > currentCmdData.MinutesUntilPowerOn && !rpi.isOn())
         {
             recordLogData(INFO, "Power On RPI Time Reached. Turning on Raspberry Pi.");
