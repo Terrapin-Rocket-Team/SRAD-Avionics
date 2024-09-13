14b2f17bc6612bc0497cb35103c949c18f76815f
jhauerst <114038876+jhauerst@users.noreply.github.com>
2024-09-13 19:43:21 -0400

    Reduce GSData header to two bytes

--- Spaceport/24-25/Code/Radio/lib/Message/src/GroundStation/GSData.h
+++ Spaceport/24-25/Code/Radio/lib/Message/src/GroundStation/GSData.h
@@ -3,46 +3,37 @@
 
 #include "Data.h"
 
-enum GSMessageType : uint8_t
-{
-    MSG_UNKWN = 0x00,
-    MSG_TELEM = 0x01,
-    MSG_CMD = 0x02,
-    MSG_TEXT = 0x03,
-    MSG_VIDEO = 0x04
-};
-
 class GSData : public Data
 {
 public:
     // maximum total message size
-    static const uint16_t maxSize = 0xFFFF;
+    static const uint16_t maxSize = 0x0FFF;
     // size of the header
-    static const uint8_t headerLen = 3;
+    static const uint8_t headerLen = 2;
     // maximum size of the data
     static const uint16_t maxDataSize = maxSize - headerLen;
 
-    // type of message, first byte of header
-    GSMessageType type = MSG_UNKWN;
+    // type of message, first byte of header, 0x00 is default, max is 16
+    uint8_t index = 0x00;
     // size of message, second and third bytes of header
-    uint16_t size = 0x0000; // length of the message (2^16 should be enough)
+    uint16_t size = 0x0000; // length of the message (2^12 = 4096 btyes should be enough)
     // buffer to store message data (not including header)
     uint8_t buf[maxDataSize] = {0}; // leave space for header
 
     // GSData default constructor
     GSData() {};
 
-    static bool decodeHeader(uint32_t header, GSMessageType &type, uint16_t &size);
+    static bool decodeHeader(uint16_t header, uint8_t &streamIndex, uint16_t &size);
 
     // GSData constructor
     // - type : the type of the message
-    GSData(GSMessageType type);
+    GSData(uint8_t streamIndex);
 
     // GSData constructor
     // - type : the type of the message
     // - buf : the data for the message
     // - size : the size of the data
-    GSData(GSMessageType type, uint8_t *buf, uint16_t size);
+    GSData(uint8_t streamIndex, uint8_t *buf, uint16_t size);
 
     // encode the data stored in the ```Data``` object and place the result in ```data```
     uint16_t encode(uint8_t *data, uint16_t sz) override;
