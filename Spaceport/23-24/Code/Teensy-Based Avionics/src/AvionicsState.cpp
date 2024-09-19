33b7ec86c796cfd98619b19050594f1337b7e521
Drew Brandt <ahbrandt@umd.edu>
2024-09-19 14:45:31 -0400

    Migrated to MMFS without KF

--- Spaceport/23-24/Code/Teensy-Based Avionics/src/AvionicsState.cpp
+++ Spaceport/23-24/Code/Teensy-Based Avionics/src/AvionicsState.cpp
@@ -3,7 +3,7 @@
 
 using namespace mmfs;
 
-AvionicsState::AvionicsState(Sensor **sensors, int numSensors, KalmanInterface *kfilter, bool recordData) : State(sensors, numSensors, kfilter, recordData)
+AvionicsState::AvionicsState(Sensor **sensors, int numSensors, Filter *filter, Logger *logger, bool stateRecordsOwnData = true) : State(sensors, numSensors, filter, logger, stateRecordsOwnData)
 {
     stage = 0;
     timeOfLaunch = 0;
@@ -25,8 +25,8 @@ void AvionicsState::determineStage()
     GPS *gps = reinterpret_cast<GPS *>(getSensor(GPS_));
     if (stage == 0 &&
         (sensorOK(imu) || sensorOK(baro)) &&
-        (sensorOK(imu) ? abs(imu->getAcceleration().z()) > 25 : true) &&
-        (sensorOK(baro) ? baro->getRelAltFt() > 60 : true))
+        (sensorOK(imu) ? abs(imu->getAccelerationGlobal().z()) > 25 : true) &&
+        (sensorOK(baro) ? baro->getAGLAltFt() > 60 : true))
     // if we are in preflight AND
     // we have either the IMU OR the barometer AND
     // imu is ok AND the z acceleration is greater than 29 ft/s^2 OR imu is not ok AND
@@ -35,19 +35,19 @@ void AvionicsState::determineStage()
     // essentially, if we have either sensor and they meet launch threshold, launch. Otherwise, it will never detect a launch.
     {
         bb.aonoff(33, 200);
-        setRecordMode(FLIGHT);
+        logger->setRecordMode(FLIGHT);
         stage = 1;
         timeOfLaunch = currentTime;
         timeOfLastStage = currentTime;
-        recordLogData(INFO, "Launch detected.");
-        recordLogData(INFO, "Printing static data.");
+        logger->recordLogData(INFO_, "Launch detected.");
+        logger->recordLogData(INFO_, "Printing static data.");
         for (int i = 0; i < maxNumSensors; i++)
         {
             if (sensorOK(sensors[i]))
             {
                 char logData[200];
                 snprintf(logData, 200, "%s: %s", sensors[i]->getName(), sensors[i]->getStaticDataString());
-                recordLogData(INFO, logData);
+                logger->recordLogData(INFO_, logData);
                 sensors[i]->setBiasCorrectionMode(false);
             }
         }
@@ -57,35 +57,35 @@ void AvionicsState::determineStage()
         bb.aonoff(33, 200, 2);
         timeOfLastStage = currentTime;
         stage = 2;
-        recordLogData(INFO, "Coasting detected.");
+        logger->recordLogData(INFO_, "Coasting detected.");
     }
     else if (stage == 2 && baroVelocity <= 0 && timeSinceLaunch > 5)
     {
         bb.aonoff(33, 200, 3);
         char logData[100];
         snprintf(logData, 100, "Apogee detected at %.2f m.", position.z());
-        recordLogData(INFO, logData);
+        logger->recordLogData(INFO_, logData);
         timeOfLastStage = currentTime;
         stage = 3;
-        recordLogData(INFO, "Drogue conditions detected.");
+        logger->recordLogData(INFO_, "Drogue conditions detected.");
     }
-    else if (stage == 3 && baro->getRelAltFt() < 1000 && timeSinceLaunch > 10)
+    else if (stage == 3 && baro->getAGLAltFt() < 1000 && timeSinceLaunch > 10)
     {
         bb.aonoff(33, 200, 4);
         stage = 4;
         timeOfLastStage = currentTime;
-        recordLogData(INFO, "Main parachute conditions detected.");
+        logger->recordLogData(INFO_, "Main parachute conditions detected.");
     }
-    else if (stage == 4 && baroVelocity > -1 && baro->getRelAltFt() < 66 && timeSinceLaunch > 15)
+    else if (stage == 4 && baroVelocity > -1 && baro->getAGLAltFt() < 66 && timeSinceLaunch > 15)
     {
         bb.aonoff(33, 200, 5);
         timeOfLastStage = currentTime;
         stage = 5;
-        recordLogData(INFO, "Landing detected. Waiting for 5 seconds to dump data.");
+        logger->recordLogData(INFO_, "Landing detected. Waiting for 5 seconds to dump data.");
     }
     else if (stage == 5 && currentTime - timeOfLastStage > 5)
     {
-        setRecordMode(GROUND);
-        recordLogData(INFO, "Dumped data after landing.");
+        logger->setRecordMode(GROUND);
+        logger->recordLogData(INFO_, "Dumped data after landing.");
     }
 }
