d5a560ae33f819fe77ce8bef0459e6e699009ed0
Varun Unnithan <varun.unnithan33@gmail.com>
2024-09-25 12:06:29 -0400

    Basic benchmarking code

--- Spaceport/Code/Kalman-Filter/Matrix-Benchmark/src/main.cpp
+++ Spaceport/Code/Kalman-Filter/Matrix-Benchmark/src/main.cpp
@@ -12,7 +12,23 @@ void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void benchmark_inverse(int matrix_size, int iterations) {
+    unsigned long startTime, endTime;
+    unsigned long totalDuration = 0;
+
+    for (int i = 0; i < iterations; i++) {
+        Matrix mat = Matrix::random(matrix_size, matrix_size);  // Assuming random matrix generator
+        startTime = micros();
+        mat.inverse();
+        endTime = micros();
+        totalDuration += (endTime - startTime);
+    }
+
+    Serial.print("Average time for matrix size ");
+    Serial.print(matrix_size);
+    Serial.print("x");
+    Serial.print(matrix_size);
+    Serial.print(" = ");
+    Serial.print(totalDuration / iterations);
+    Serial.println(" microseconds");
 }
