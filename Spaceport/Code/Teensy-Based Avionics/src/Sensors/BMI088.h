5145a002f7f78ff64503735f1652fb7b4d895334
Ramy Kaddouri <myramkadd@gmail.com>
2024-09-26 16:26:29 -0400

    make naming consistent

--- Spaceport/Code/Teensy-Based Avionics/src/Sensors/BMI088.h
+++ Spaceport/Code/Teensy-Based Avionics/src/Sensors/BMI088.h
@@ -7,18 +7,15 @@
 #include <BMI088.h>
 #include <Sensors/IMU/IMU.h>
 
-class BMI_088 : public mmfs::IMU {
+class BMI088 : public mmfs::IMU {
 public:
-    BMI_088(TwoWire &bus,uint8_t address) : accel(bus, address), gyro(bus, address) {
+    BMI088(TwoWire &bus,uint8_t address) : accel(bus, address), gyro(bus, address) {
         Sensor::setName("BMI088");
     }
-    ~BMI_088() override;
+    ~BMI088() override;
 
     bool init() override;
     void read() override;
-
-    mmfs::Vector<3> getGyroReading() override;
-    mmfs::Vector<3> getAccReading() override;
 
 protected:
     Bmi088Accel accel;
