c93b390e12da9aeb0cddedde4162707eb4efd024
Ramy Kaddouri <myramkadd@gmail.com>
2024-09-26 16:26:29 -0400

    make naming consistent

--- Spaceport/Code/Teensy-Based Avionics/src/Sensors/BMI088.cpp
+++ Spaceport/Code/Teensy-Based Avionics/src/Sensors/BMI088.cpp
@@ -2,27 +2,19 @@
 // Created by ramykaddouri on 9/24/24.
 //
 
-#include "BMI_088.h"
+#include "BMI088.h"
 
-BMI_088::~BMI_088() {
+BMI088::~BMI088() {
 }
 
-bool BMI_088::init() {
+bool BMI088::init() {
     int accelStatus = accel.begin();
     int gyroStatus = gyro.begin();
 
     initialized = (accelStatus > 0 && gyroStatus > 0)
 }
 
-mmfs::Vector<3> BMI_088::getGyroReading() {
-    return IMU::getGyroReading();
-}
-
-mmfs::Vector<3> BMI_088::getAccReading() {
-    return IMU::getAccReading();
-}
-
-void BMI_088::read() {
+void BMI088::read() {
     accel.readSensor();
     gyro.readSensor();
 
