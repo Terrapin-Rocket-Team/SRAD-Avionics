f69e5b250825fa295c38a1f6692e7047b0280ace
Ramy Kaddouri <myramkadd@gmail.com>
2024-09-26 16:51:31 -0400

    comment

--- Spaceport/Code/Teensy-Based Avionics/src/Sensors/BMI088.h
+++ Spaceport/Code/Teensy-Based Avionics/src/Sensors/BMI088.h
@@ -10,6 +10,7 @@
 
 class BMI088 : public mmfs::IMU {
 public:
+    //See https://github.com/bolderflight/bmi088-arduino/blob/main/README.md for default addresses
     BMI088(const char* name = "BMI088", TwoWire &bus = Wire, uint8_t accelAddr = 0x18, uint8_t gyroAddr = 0x68) : accel(bus, accelAddr), gyro(bus, gyroAddr) {
         Sensor::setName(name);
     }
