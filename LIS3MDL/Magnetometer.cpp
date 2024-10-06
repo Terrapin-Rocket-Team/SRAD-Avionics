ae300c1f9037ce2277f27ac2f9178b54046ff887
Pocash05 <ofreidli@terpmail.umd.edu>
2024-10-06 18:06:03 -0400

    Update Magnetometer.cpp

--- LIS3MDL/Magnetometer.cpp
+++ LIS3MDL/Magnetometer.cpp
@@ -2,13 +2,26 @@
 #include "IMU.h"
 
 
+int main()
+
+
+
 class LIS: public IMU {
 
 public:
     
+    float Field;
+
+    void printField{
+        
+    }
+
 }
 
 
+void LIS::read(){
+    
+}
 
 
 
