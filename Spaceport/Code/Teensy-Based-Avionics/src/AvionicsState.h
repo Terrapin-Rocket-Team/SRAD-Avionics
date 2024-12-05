f992d8bca48732e96075e87871921e49e66fc120
Drew Brandt <ahbrandt@umd.edu>
2024-12-04 19:48:33 -0500

    fix record data

--- Spaceport/Code/Teensy-Based-Avionics/src/AvionicsState.h
+++ Spaceport/Code/Teensy-Based-Avionics/src/AvionicsState.h
@@ -10,11 +10,12 @@ class AvionicsState : public State
 public:
     AvionicsState(Sensor **sensors, int numSensors, LinearKalmanFilter *kfilter);
     void updateState(double newTime = -1) override;
+    uint32_t getStage() { return stage; }
 
 private:
     char stages[6][15] = {"Pre-Flight", "Boosting", "Coasting", "Drogue Descent", "Main Descent", "Post-Flight"};
     void determineStage();
-    int stage;
+    uint32_t stage;
     double timeOfLaunch;
     double timeOfLastStage;
     double timeOfDay;
