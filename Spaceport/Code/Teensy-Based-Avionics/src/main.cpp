0c60f6a9d1ea709edaeeb4fbf2f92b0b8399076f
jhauerst <jhauerst@terpmail.umd.edu>
2025-01-08 15:06:26 -0500

    add sled STL and code to combine radio and fc

--- Spaceport/Code/Teensy-Based-Avionics/src/main.cpp
+++ Spaceport/Code/Teensy-Based-Avionics/src/main.cpp
@@ -5,6 +5,7 @@
 #include "Pi.h"
 #include "AvionicsKF.h"
 #include "RadioMessage.h"
+#include "Si4463.h"
 
 #define BMP_ADDR_PIN 36
 #define RPI_PWR 0
@@ -23,6 +24,28 @@ APRSConfig aprsConfig = {"KC3UTM", "ALL", "WIDE1-1", PositionWithoutTimestampWit
 APRSTelem aprs(aprsConfig);
 Message msg;
 
+Si4463HardwareConfig hwcfg = {
+    MOD_2GFSK, // modulation
+    DR_500b,   // data rate
+    433e6,     // frequency (Hz)
+    127,       // tx power (127 = ~20dBm)
+    48,        // preamble length
+    16,        // required received valid preamble
+};
+
+Si4463PinConfig pincfg = {
+    &SPI, // spi bus to use
+    8,    // cs
+    6,    // sdn
+    7,    // irq
+    9,    // gpio0
+    10,   // gpio1
+    4,    // random pin - gpio2 is not connected
+    5,    // random pin - gpio3 is not connected
+};
+
+Si4463 radio(hwcfg, pincfg);
+
 Sensor *sensors[4] = {&gps, &bno, &baro1, &baro2};
 AvionicsKF kfilter;
 
@@ -33,6 +56,7 @@ PSRAM *psram;
 ErrorHandler errorHandler;
 
 static double last = 0; // for better timing than "delay(100)"
+bool gpsHasFix = false;
 
 // Free memory debug function
 extern unsigned long _heap_start;
@@ -48,7 +72,7 @@ void FreeMem()
 }
 // Free memory debug function
 
-const int BUZZER_PIN = 33;
+const int BUZZER_PIN = 0;
 const int BUILTIN_LED_PIN = LED_BUILTIN;
 int allowedPins[] = {BUILTIN_LED_PIN, BUZZER_PIN, 32};
 BlinkBuzz bb(allowedPins, 3, true);
@@ -59,7 +83,7 @@ const int UPDATE_INTERVAL = 1000.0 / UPDATE_RATE;
 void setup()
 {
     Serial.begin(9600);
-    Serial1.begin(460800);
+    Serial1.begin(115200);
     delay(3000);
     Wire.begin();
     SENSOR_BIAS_CORRECTION_DATA_LENGTH = 2;
@@ -109,6 +133,15 @@ void setup()
     }
     logger.writeCsvHeader();
     bb.aonoff(32, *(new BBPattern(200, 1)), true); // blink a status LED (until GPS fix)
+
+    if (!radio.begin())
+    {
+        Serial.println("Error: radio failed to begin");
+        Serial.flush();
+        while (1)
+            ;
+    }
+    Serial.println("Radio began successfully");
 }
 double radio_last;
 void loop()
@@ -124,10 +157,17 @@ void loop()
 
     // printf("%.2f | %.2f = %.2f | %.2f\n", baro1.getASLAltFt(), baro2.getASLAltFt(), baro1.getAGLAltFt(), baro2.getAGLAltFt());
     logger.recordFlightData();
-    if (gps.getHasFirstFix())
+    if (gps.getFixQual() > 0 && !gpsHasFix)
     {
+        gpsHasFix = true;
         bb.clearQueue(32);
         bb.on(32);
+    }
+    else if (gpsHasFix)
+    {
+        gpsHasFix = false;
+        bb.clearQueue(32);
+        bb.aonoff(32, *(new BBPattern(200, 1)), true); // blink a status LED (until GPS fix)
     }
 
     Vector<3> orient = bno.getOrientation().toEuler() * 180 / PI;
@@ -150,13 +190,16 @@ void loop()
     aprs.stateFlags = computer->getStage();
     msg.encode(&aprs);
     Message m(&aprs);
-    APRSTelem aprs2(aprsConfig);
-    m.decode(&aprs2);
-    Serial1.write(msg.buf, msg.size);
-    Serial1.write('\n');
+    // APRSTelem aprs2(aprsConfig);
+    // m.decode(&aprs2);
+    Serial.write(msg.buf, msg.size);
+    Serial.write('\n');
+    radio.send(aprs);
+    // Serial1.write(msg.buf, msg.size);
+    // Serial1.write('\n');
     // printf("%0.7f | %0.7f | %d\n", aprs2.lat, aprs.lng, gps.getFixQual());
 
-    // Serial.println(gps.getFixQual());
+    Serial.println(gps.getFixQual());
 
     // time, alt1, alt2, vel, accel, gyro, mag, lat, lon
     // printf("%.3f | %.2f, %.2f, %.2f | %.2f, %.2f, %.2f | %.2f, %.2f, %.2f \n",
