e8dd07ec65e61ffefbcd1db1473547ba040d4c68
Drew Brandt <ahbrandt@umd.edu>
2025-01-29 16:12:47 -0500

    fixes based on tests

--- Spaceport/Code/Teensy-Based-Avionics/src/AvionicsState.cpp
+++ Spaceport/Code/Teensy-Based-Avionics/src/AvionicsState.cpp
@@ -20,7 +20,7 @@ void AvionicsState::determineStage()
     if (stage == 0 &&
         (sensorOK(imu) || sensorOK(baro)) &&
         //(sensorOK(imu) ? abs(imu->getAccelerationGlobal().z()) > 25 : true) &&
-        (sensorOK(baro) ? baro->getAGLAltFt() > 3 : true))
+        (sensorOK(baro) ? baro->getAGLAltFt() > 5 : true))
     // if we are in preflight AND
     // we have either the IMU OR the barometer AND
     // imu is ok AND the z acceleration is greater than 29 ft/s^2 OR imu is not ok AND
@@ -77,7 +77,7 @@ void AvionicsState::determineStage()
         stage = 5;
         getLogger().recordLogData(INFO_, "Landing detected. Waiting for 5 seconds to dump data.");
     }
-    else if ((stage == 5 && currentTime - timeOfLastStage > 5) || (stage >= 1 && timeSinceLaunch > 5 * 60))
+    else if ((stage == 5 && currentTime - timeOfLastStage > 5) || (stage >= 1 && stage != 6 && timeSinceLaunch > 5 * 60))
     {
         stage = 6;
         getLogger().setRecordMode(GROUND);
