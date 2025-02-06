6e87b6793f96a842b387380deb1a7257ba9e8f04
Ramy Kaddouri <myramkadd@gmail.com>
2025-02-05 23:42:47 -0500

    create client and server classes for now

---
+++ Spaceport/Code/ESP32-Bluetooth/src/BluetoothClient.h
@@ -0,0 +1,16 @@
+//
+// Created by ramykaddouri on 2/5/25.
+//
+
+#ifndef BLUETOOTHCLIENT_H
+#define BLUETOOTHCLIENT_H
+
+
+
+class BluetoothClient {
+
+};
+
+
+
+#endif //BLUETOOTHCLIENT_H
---
+++ Spaceport/Code/ESP32-Bluetooth/src/BluetoothServer.h
@@ -0,0 +1,16 @@
+//
+// Created by ramykaddouri on 2/5/25.
+//
+
+#ifndef BLUETOOTHSERVER_H
+#define BLUETOOTHSERVER_H
+
+
+
+class BluetoothServer {
+
+};
+
+
+
+#endif //BLUETOOTHSERVER_H
