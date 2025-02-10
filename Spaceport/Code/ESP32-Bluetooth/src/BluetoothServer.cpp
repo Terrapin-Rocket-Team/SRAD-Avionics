a26de96950af3de78323a69d787a74f23491a70c
Ramy Kaddouri <myramkadd@gmail.com>
2025-02-09 20:00:49 -0500

    start implementing server

--- Spaceport/Code/ESP32-Bluetooth/src/BluetoothServer.cpp
+++ Spaceport/Code/ESP32-Bluetooth/src/BluetoothServer.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "BluetoothServer.h"
+
+BluetoothServer::BluetoothServer(const std::string &name) : name(name) {
+    BLEDevice::init(name);
+
+    pServer = BLEDevice::createServer();
+    //TODO: There should probably be a better way to do these UUIDs
+    pService = pServer->createService(name + "-service");
+    pCharacteristic = pService->createCharacteristic(
+        name + "-characterisitc0",
+        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
+    );
+    pCharacteristic->setCallbacks(this);
+}
+
+BluetoothServer::~BluetoothServer() {
+    BLEDevice::deinit(true);
+}
+
+bool BluetoothServer::start() {
+    if (pService != nullptr) {
+        pService->start();
+        pAdvertising = pServer->getAdvertising();
+        pAdvertising->start();
+        return true;
+    }
+
+    return false;
+}
+
+void BluetoothServer::onRead(BLECharacteristic *pCharacteristic, esp_ble_gatts_cb_param_t *param) {
+    //TODO: implement
+}
+
+void BluetoothServer::onWrite(BLECharacteristic *pCharacteristic, esp_ble_gatts_cb_param_t *param) {
+    //TODO: implement
+}
+
+void BluetoothServer::onNotify(BLECharacteristic *pCharacteristic) {
+    //TODO: implement
+}
+
+void BluetoothServer::onStatus(BLECharacteristic *pCharacteristic, Status s, uint32_t code) {
+    //TODO: implement
+}
