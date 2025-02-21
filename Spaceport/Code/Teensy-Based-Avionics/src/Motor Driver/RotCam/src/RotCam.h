7988d0c1521ebd90561202d3f5e014df22a1e1b2
Pocash05 <ofreidli@terpmail.umd.edu>
2025-02-21 15:11:37 -0500

    Platformio Code for Motor Driver

---
+++ Spaceport/Code/Teensy-Based-Avionics/src/Motor Driver/RotCam/src/RotCam.h
@@ -0,0 +1,33 @@
+#ifndef ROTCAM_H
+#define ROTCAM_H
+
+#include <Arduino.h>
+#include <AccelStepper.h>
+#define HOME_SENS_PIN 23
+#define STEPPER_PIN_1 38
+#define STEPPER_PIN_2 39
+#define STEPPER_PIN_3 40
+#define STEPPER_PIN_4 41
+
+
+class RotCam
+{
+private:
+    AccelStepper stepper;
+    int stepsPerRevolution;
+    bool detect();
+    long move(bool positive = true);
+    float targetAngle;
+
+public:
+    RotCam();
+    void moveToAngle(float angle, bool positive = true);
+    void home();
+    int findStepsPerRevolution();
+    void run();
+    int getStepsPerRevolution();
+
+
+};
+
+#endif
