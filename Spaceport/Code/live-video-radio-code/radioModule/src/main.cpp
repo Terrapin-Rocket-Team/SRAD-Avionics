f49cc04bbd36cd2419b0785e179b78d7d9abba8d
Louie Shapiro <lshapir6@terpmail.umd.edu>
2025-03-01 16:32:55 -0500

    Push

--- Spaceport/Code/live-video-radio-code/radioModule/src/main.cpp
+++ Spaceport/Code/live-video-radio-code/radioModule/src/main.cpp
@@ -1,15 +1,24 @@
 #include <Arduino.h>
+#include <SPI.h>
 
-int data = 0;
+// Si4463 Pin Assignments
+#define Si_IRQ 0
+
+void Si_IRQ_handler() {
+  
+}
 
 void setup() {
-  pinMode(2, INPUT); // IRQ Pin on Si4463
   Serial1.begin(9600);
+  SPI.begin();  // Teensy SPI bus 0
+  SPI1.begin(); // Teensy SPI bus 1
+  pinMode(Si_IRQ, INPUT_PULLUP);
+
 }
 
 void loop() {
-  while (Serial1.available() > 0) {
-    data = Serial1.read();
+  while (Serial1.available()) {
+    
   }
 }
 
