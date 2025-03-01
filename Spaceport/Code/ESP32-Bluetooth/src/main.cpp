817763ae492fa4982253e2a688b17de1b0ceb1cc
varun unnithan <varun.unnithan33@gmail.com>
2025-02-28 19:15:41 -0500

    added debug options

--- Spaceport/Code/ESP32-Bluetooth/src/main.cpp
+++ Spaceport/Code/ESP32-Bluetooth/src/main.cpp
@@ -8,7 +8,7 @@
 #include <BluetoothServer.h>
 
 // #define SERVER
-// #define DEBUG
+#define DEBUG
 
 #ifdef SERVER
 BluetoothServer server(Serial);
@@ -17,13 +17,23 @@ BluetoothClient client(Serial);
 #endif
 
 void setup() {
-    Serial.begin(9600);
 #ifdef DEBUG
+    Serial.begin(9600);
+#endif
+
 #ifdef SERVER
     server.start("ESP32 BLE Server");
+    #ifdef DEBUG
+    Serial.println("Server started");
+    #endif
 #else
+    #ifdef DEBUG
+    Serial.println("Trying to start client");
+    #endif
     client.start("ESP32 BLE Server");
-#endif
+    #ifdef DEBUG
+    Serial.println("Client started");
+    #endif
 #endif
 }
 
@@ -36,26 +46,51 @@ void serverLoop() {
             case INIT_MESSAGE: {
                 std::string name = Serial.readString().c_str();
                 server.start(name);
+                #ifdef DEBUG
+                Serial.println("Received INIT_MESSAGE");
+                #endif
                 break;
             }
             case DATA_MESSAGE: {
                 if (server.isInitialized()) {
                     server.update(Serial);
+                    #ifdef DEBUG
+                    Serial.println("Received DATA_MESSAGE");
+                    #endif
                 }
                 break;
             }
             default: {
+                #ifdef DEBUG
+                Serial.println("Received unknown message");
+                #endif
                 break;
             };
         }
     }
+    #ifdef DEBUG
+    Serial.println("Server loop");
+    #endif
 }
 #endif
 
 #ifndef SERVER
+void sendHelloWorld() {
+    if (client.isConnected()) {
+        const char* message = "Hello World";
+        client.send((uint8_t*)message, strlen(message));
+        #ifdef DEBUG
+        Serial.println("Sent Hello World");
+        #endif
+    }
+}
+
 void clientLoop() {
     if (!client.isConnected()) {
         client.update(Serial);
+        #ifdef DEBUG
+        Serial.println("Client not connected");
+        #endif
     } else {
         if (Serial.available()) {
             uint8_t messageID = Serial.read();
@@ -77,6 +112,10 @@ void clientLoop() {
                 };
             }
         }
+        #ifdef DEBUG
+        sendHelloWorld(); // Send "Hello World" message
+        delay(1000); // Delay to avoid flooding the messages
+        #endif
     }
 }
 #endif
