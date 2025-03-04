5f71b4589c43b700ecc9d47483234328de2bb4ad
Drew Brandt <ahbrandt@umd.edu>
2025-03-04 14:11:11 -0500

    test

--- Spaceport/Code/Teensy-Based-Avionics/src/main.cpp
+++ Spaceport/Code/Teensy-Based-Avionics/src/main.cpp
@@ -1,153 +1,66 @@
-#include <Arduino.h>
-#include "BMI088.h"
+/*
+  LittleFS  datalogger
+ 
+ This example shows how to log data from three analog sensors
+ to an storage device such as a FLASH.
+ 
+ This example code is in the public domain.
+ */
 
-#include "AvionicsState.h"
-#include "Pi.h"
-#include "AvionicsKF.h"
-#include "RadioMessage.h"
-#include "Si4463.h"
-#include "AviEventListener.h"
-#include <MMFS.h>
+ #include <LittleFS.h>
 
-#define RPI_PWR 0
-#define RPI_VIDEO 1
-
-using namespace mmfs;
-
-MAX_M10S gps;
-mmfs::DPS310 baro1;
-mmfs::BMI088andLIS3MDL bno;
-Sensor *sensors[3] = {&gps, &bno, &baro1};
-AvionicsKF kfilter;
-AvionicsState computer(sensors, 3, &kfilter);
-
-APRSConfig aprsConfig = {"KC3UTM", "ALL", "WIDE1-1", PositionWithoutTimestampWithoutAPRS, '\\', 'M'};
-uint8_t encoding[] = {7, 4, 4};
-APRSTelem aprs(aprsConfig);
-
-Message msg;
-
-Si4463HardwareConfig hwcfg = {
-    MOD_2GFSK, // modulation
-    DR_500b,   // data rate
-    433e6,     // frequency (Hz)
-    5,       // tx power (127 = ~20dBm)
-    48,        // preamble length
-    16,        // required received valid preamble
-};
-
-Si4463PinConfig pincfg = {
-    &SPI, // spi bus to use
-    10,   // cs
-    20,   // sdn
-    23,   // irq
-    22,   // gpio0
-    21,   // gpio1
-    36,   // random pin - gpio2 is not connected
-    37,   // random pin - gpio3 is not connected
-};
-
-Si4463 radio(hwcfg, pincfg);
-uint32_t radioTimer = millis();
-Pi rpi(RPI_PWR, RPI_VIDEO);
-
-// Free memory debug function
-extern unsigned long _heap_start;
-extern unsigned long _heap_end;
-extern char *__brkval;
-
-void FreeMem()
-{
-    void *heapTop = malloc(500);
-    Serial.print((long)heapTop);
-    Serial.print("\n");
-    free(heapTop);
-}
-// Free memory debug function
-
-MMFSConfig config = MMFSConfig()
-                        .withBBPin(LED_BUILTIN)
-                        .withBBPin(32)
-                        .withBuzzerPin(33)
-                        .withState(&computer)
-                        .withUsingSensorBiasCorrection(true)
-                        .withUpdateRate(20)
-                        .withBBAsync(true, 50);
-
-MMFSSystem sys(&config);
-
-AviEventLister listener;
-
-void setup()
-{
-    // Serial1.begin(115200);
-    sys.init();
-    bb.aonoff(32, *(new BBPattern(200, 1)), true); // blink a status LED (until GPS fix)
-
-    if (radio.begin())
-    {
-        bb.onoff(BUZZER, 1000);
-        getLogger().recordLogData(ERROR_, "Radio initialized.");
-    }
-    else
-    {
-        bb.onoff(BUZZER, 200, 3);
-        getLogger().recordLogData(INFO_, "Radio failed to initialize.");
-    }
-
-    getLogger().recordLogData(INFO_, "Initialization Complete");
-
-    // Serial8.begin(115200);
-    // getLogger().recordLogData(INFO_, "RotCam Serial initialized.");
-
-    // delay(1000);
-    // if (Serial8.availableForWrite() > 0)
-    // {
-    //     Serial8.println("90");
-    //     getLogger().recordLogData(INFO_, "RotCam rotated to 90 degrees.");
-    // }
-}
-double radio_last;
-void loop()
-{
-    if (sys.update())
-    {
-        // FreeMem();
-    }
-    radio.update();
-
-    double time = millis();
-    if (time - radio_last < 1000)
-        return;
-
-    radio_last = time;
-    msg.clear();
-
-    /// printf("%f\n", baro1.getAGLAltFt());
-    aprs.alt = baro1.getAGLAltFt();
-    // printf("%f\n", gps.getHeading());
-    aprs.hdg = gps.getHeading();
-    // printf("%f\n", gps.getPos().x());
-    aprs.lat = gps.getPos().x();
-    // printf("%f\n", gps.getPos().y());
-    aprs.lng = gps.getPos().y();
-    // printf("%f\n", computer.getVelocity().z());
-    aprs.spd = computer.getVelocity().z();
-    // printf("%f\n", bno.getAngularVelocity().x());
-    aprs.orient[0] = bno.getAngularVelocity().x();
-    // printf("%f\n", bno.getAngularVelocity().y());
-    aprs.orient[1] = bno.getAngularVelocity().y();
-    // printf("%f\n", bno.getAngularVelocity().z());
-    aprs.orient[2] = bno.getAngularVelocity().z();
-    aprs.stateFlags.setEncoding(encoding, 3);
-
-    uint8_t arr[] = {(uint8_t)(int)baro1.getTemp(), (uint8_t)computer.getStage(), (uint8_t)gps.getFixQual()};
-    aprs.stateFlags.pack(arr);
-    // aprs.stateFlags = (uint8_t) computer.getStage();
-    msg.encode(&aprs);
-    radio.send(aprs);
-    Serial.printf("%0.3f - Sent APRS Message; %f   |   %d\n", time / 1000.0, baro1.getAGLAltFt(), gps.getFixQual());
-    bb.aonoff(BUZZER, 50);
-    // Serial1.write(msg.buf, msg.size);
-    // Serial1.write('\n');
-}
+ LittleFS_QPINAND  myfs;   //Specifies to use an SPI NOR Flash attached to SPI
+ 
+ const int chipSelect = 4;
+ 
+ void setup()
+ {
+ 
+   // Open serial communications and wait for port to open:
+   Serial.begin(9600);
+   while (!Serial) {
+     // wait for serial port to connect.
+   }
+ 
+ 
+   Serial.print("Initializing SPI FLASH...");
+ 
+   // see if the Flash is present and can be initialized:
+  if (!myfs.begin()) {
+     Serial.printf("Error starting %s\n", "SPI FLASH");
+     while (1) {
+       // Flash error, so don't do anything more - stay stuck here
+     }
+   }
+   Serial.println("Flash initialized.");
+ }
+ 
+ void loop()
+ {
+   // make a string for assembling the data to log:
+   String dataString = "";
+ 
+   // read three sensors and append to the string:
+   for (int analogPin = 0; analogPin < 3; analogPin++) {
+     int sensor = analogRead(analogPin);
+     dataString += String(sensor);
+     if (analogPin < 2) {
+       dataString += ",";
+     }
+   }
+ 
+   // open the file.
+   File dataFile = myfs.open("datalog.txt", FILE_WRITE);
+   
+   // if the file is available, write to it:
+   if (dataFile) {
+     dataFile.println(dataString);
+     dataFile.close();
+     // print to the serial port too:
+     Serial.println(dataString);
+   } else {
+     // if the file isn't open, pop up an error:
+     Serial.println("error opening datalog.txt");
+   }
+   delay(100); // run at a reasonable not-too-fast speed
+ }
