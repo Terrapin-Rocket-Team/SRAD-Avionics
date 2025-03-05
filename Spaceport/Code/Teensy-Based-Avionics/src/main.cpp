c8c4d7aea6765c9070c6497c4d51278b7f54b058
Drew Brandt <ahbrandt@umd.edu>
2025-03-05 13:02:16 -0500

    idk but fixes GPS

--- Spaceport/Code/Teensy-Based-Avionics/src/main.cpp
+++ Spaceport/Code/Teensy-Based-Avionics/src/main.cpp
@@ -1,30 +1,30 @@
 #include <Arduino.h>
-#include "BMI088.h"
-
-#include "AvionicsState.h"
-#include "Pi.h"
-#include "AvionicsKF.h"
-#include "RadioMessage.h"
-#include "Si4463.h"
-#include "AviEventListener.h"
 #include <MMFS.h>
+#include "AvionicsState.h"
+#include "AvionicsKF.h"
+#include "AviEventListener.h"
+#include "Pi.h"
+#include "Si4463.h"
 
 #define RPI_PWR 0
 #define RPI_VIDEO 1
 
 using namespace mmfs;
 
-MAX_M10S gps;
-mmfs::DPS310 baro1;
-mmfs::BMI088andLIS3MDL bno;
-Sensor *sensors[3] = {&gps, &bno, &baro1};
-AvionicsKF kfilter;
-AvionicsState computer(sensors, 3, &kfilter);
+
+MAX_M10S m;
+DPS310 d;
+BMI088andLIS3MDL b;
+
+Sensor *s[] = {&m, &d, &b};
+AvionicsKF fk;
+AvionicsState t(s, sizeof(s) / 4, &fk);
+
+
 
 APRSConfig aprsConfig = {"KC3UTM", "ALL", "WIDE1-1", PositionWithoutTimestampWithoutAPRS, '\\', 'M'};
 uint8_t encoding[] = {7, 4, 4};
 APRSTelem aprs(aprsConfig);
-
 Message msg;
 
 Si4463HardwareConfig hwcfg = {
@@ -51,7 +51,6 @@ Si4463 radio(hwcfg, pincfg);
 uint32_t radioTimer = millis();
 Pi rpi(RPI_PWR, RPI_VIDEO);
 
-// Free memory debug function
 extern unsigned long _heap_start;
 extern unsigned long _heap_end;
 extern char *__brkval;
@@ -63,59 +62,31 @@ void FreeMem()
     Serial.print("\n");
     free(heapTop);
 }
-// Free memory debug function
 
-MMFSConfig config = MMFSConfig()
-                        .withBBPin(LED_BUILTIN)
-                        .withBBPin(32)
-                        .withBuzzerPin(33)
-                        .withState(&computer)
-                        .withUsingSensorBiasCorrection(true)
-                        .withUpdateRate(20)
-                        .withBBAsync(true, 50);
-
-MMFSSystem sys(&config);
+MMFSConfig a = MMFSConfig()
+                   .withBBAsync(true, 50)
+                   .withBBPin(LED_BUILTIN)
+                   .withBBPin(32)
+                   .withUsingSensorBiasCorrection(true)
+                   .withUpdateRate(10)
+                   .withState(&t);
+MMFSSystem sys(&a);
 
 AviEventLister listener;
 
 void setup()
 {
-    // Serial1.begin(115200);
     sys.init();
     bb.aonoff(32, *(new BBPattern(200, 1)), true); // blink a status LED (until GPS fix)
-
-    if (radio.begin())
-    {
-        bb.onoff(BUZZER, 1000);
-        getLogger().recordLogData(ERROR_, "Radio initialized.");
-    }
-    else
-    {
-        bb.onoff(BUZZER, 200, 3);
-        getLogger().recordLogData(INFO_, "Radio failed to initialize.");
-    }
-
     getLogger().recordLogData(INFO_, "Initialization Complete");
-
-    // Serial8.begin(115200);
-    // getLogger().recordLogData(INFO_, "RotCam Serial initialized.");
-
-    // delay(1000);
-    // if (Serial8.availableForWrite() > 0)
-    // {
-    //     Serial8.println("90");
-    //     getLogger().recordLogData(INFO_, "RotCam rotated to 90 degrees.");
-    // }
 }
 double radio_last;
+
 void loop()
 {
     if (sys.update())
     {
-        // FreeMem();
     }
-    radio.update();
-
     double time = millis();
     if (time - radio_last < 1000)
         return;
@@ -124,30 +95,31 @@ void loop()
     msg.clear();
 
     /// printf("%f\n", baro1.getAGLAltFt());
-    aprs.alt = baro1.getAGLAltFt();
+    aprs.alt = d.getAGLAltFt();
     // printf("%f\n", gps.getHeading());
-    aprs.hdg = gps.getHeading();
+    aprs.hdg = m.getHeading();
     // printf("%f\n", gps.getPos().x());
-    aprs.lat = gps.getPos().x();
+    aprs.lat = m.getPos().x();
     // printf("%f\n", gps.getPos().y());
-    aprs.lng = gps.getPos().y();
+    aprs.lng = m.getPos().y();
     // printf("%f\n", computer.getVelocity().z());
-    aprs.spd = computer.getVelocity().z();
+    aprs.spd = t.getVelocity().z();
     // printf("%f\n", bno.getAngularVelocity().x());
-    aprs.orient[0] = bno.getAngularVelocity().x();
+    aprs.orient[0] = b.getAngularVelocity().x();
     // printf("%f\n", bno.getAngularVelocity().y());
-    aprs.orient[1] = bno.getAngularVelocity().y();
+    aprs.orient[1] = b.getAngularVelocity().y();
     // printf("%f\n", bno.getAngularVelocity().z());
-    aprs.orient[2] = bno.getAngularVelocity().z();
+    aprs.orient[2] = b.getAngularVelocity().z();
     aprs.stateFlags.setEncoding(encoding, 3);
 
-    uint8_t arr[] = {(uint8_t)(int)baro1.getTemp(), (uint8_t)computer.getStage(), (uint8_t)gps.getFixQual()};
+    uint8_t arr[] = {(uint8_t)(int)d.getTemp(), (uint8_t)t.getStage(), (uint8_t)m.getFixQual()};
     aprs.stateFlags.pack(arr);
     // aprs.stateFlags = (uint8_t) computer.getStage();
     msg.encode(&aprs);
-    radio.send(aprs);
-    Serial.printf("%0.3f - Sent APRS Message; %f   |   %d\n", time / 1000.0, baro1.getAGLAltFt(), gps.getFixQual());
+    // radio.send(aprs);
+    Serial.printf("%0.3f - Sent APRS Message; %f   |   %d\n", time / 1000.0, d.getAGLAltFt(), m.getFixQual());
     bb.aonoff(BUZZER, 50);
     // Serial1.write(msg.buf, msg.size);
     // Serial1.write('\n');
+
 }
