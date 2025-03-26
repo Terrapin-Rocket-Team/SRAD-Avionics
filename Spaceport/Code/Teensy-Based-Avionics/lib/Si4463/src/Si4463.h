9e44fcd4e02ec5145f77f69d08c90fae3580c53f
Drew Brandt <ahbrandt@umd.edu>
2025-03-25 23:01:08 -0400

    new radio code

--- Spaceport/Code/Teensy-Based-Avionics/lib/Si4463/src/Si4463.h
+++ Spaceport/Code/Teensy-Based-Avionics/lib/Si4463/src/Si4463.h
@@ -1,14 +1,16 @@
 #ifndef SI4463_H
 #define SI4463_H
 
-// #include "Radio.h"
-#include <Arduino.h>
+#include "Radio.h"
 #include "Si4463_defs.h"
-#include "RadioMessage.h"
 #include "SPI.h"
 
-#define PART_NO 0x4463
-#define MAX_NUM_PROPS 12
+// include the default configuration file
+#include "si4463_default.h"
+// RF4463F30 COTS radios require this to be defined
+// #define RF4463F30
+// set to 1 to always force using SPI for CTS
+#define FORCE_SPI_CTS 0
 
 /*
 Si4463 Hardware Configuration
@@ -54,38 +56,78 @@ struct Si4463PinConfig
     uint8_t gpio3;
 };
 
-class Si4463
+class Si4463 : public Radio
 {
 public:
     // the maximum length of a transmitted or received message
-    static const uint16_t maxLen = 0x1FFF;
-    // the current radio state, does not reflect hardware state
+    static const uint16_t MAX_LEN = 0x1FFF;
+    // Si4463 part number
+    static const uint16_t PART_NO = 0x4463;
+    // maximum number of properties in a single get/setProperties call
+    static const uint8_t MAX_NUM_PROPS = 12;
+    // timeout for waiting for CTS
+    static const uint8_t CTS_TIMEOUT = 100; // ms
+    // length of the FIFO in default config
+    static const uint8_t FIFO_LENGTH = 129; // bytes
+    // RX_FIFO_FULL interrupt occurs when there are more than RX_THRESH bytes in FIFO
+    static const uint8_t RX_THRESH = 63; // bytes (max 64)
+    // TX_FIFO_EMPTY interrupt occurs when there is more than TX_THRESH bytes of space in FIFO
+    static const uint8_t TX_THRESH = 63; // bytes (max 64)
+    // the current radio state, does not always align with hardware state
     Si4463State state = STATE_IDLE;
     // a Message object used to encode and decode the message
     Message m;
     // the buffer to store messages that are currently being sent or received
-    uint8_t buf[maxLen];
+    uint8_t buf[Si4463::MAX_LEN];
     // the length of the buffer
     uint16_t length = 0;
     // the number of message bytes transferred
     uint16_t xfrd = 0;
+    // pointer to the end of data in ```buf``` (TX), or the start of unread data (RX)
+    uint32_t availLen = 0;
     // the received signal strength
     int rssi = 100;
     // the modulation scheme the radio is using
     Si4463Mod mod;
     // the current radio symbol rate
     Si4463DataRate dataRate;
-    // the current transmit/receive frequency
+    // the current transmit/receive frequency = baseFreq + channel * channelSpacing
     uint32_t freq;
+    // the current channel used to set the transmit/receive frequency
+    uint8_t channel = 0;
     // the current transmit power (0-127), see datasheet
     uint8_t pwr;
     // the current preamble length in symbols
     uint8_t preambleLen;
     // the current preamble length threshold for packet detection in symbols
     uint8_t preambleThresh;
-
+    // whether a full message has been received
     bool available = false;
+    // the amount of time between attempting to read/write bytes
+    uint32_t byteDelay = 0;
 
+    uint32_t debugTimer = micros();
+
+    /*
+    Si4463 constructor, uses the following default configuration
+    Hardware:
+    - mod: MOD_2GFSK
+    - dataRate: DR_100k
+    - freq: 433 MHz
+    - pwr: 127 (+20dBm)
+    - preambleLength: 48
+    - preambleThresh: 16
+    Pin Config:
+    - spi: SPI
+    - cs: 10
+    - sdn: 38
+    - irq: 33
+    - gpio0: 34
+    - gpio1: 35
+    - gpio2: 36
+    - gpio3: 37
+    */
+    Si4463();
     /*
     Si4463 constructor
     - hConfig : the hardware configuration to initialize the radio with
@@ -93,41 +135,75 @@ public:
     */
     Si4463(Si4463HardwareConfig hConfig, Si4463PinConfig pConfig);
 
+    // Destructor
+    ~Si4463();
+
     // radio class funtions
     /*
     Begins communication with the radio, initializes and checks SPI communication and sets hardware confirguration
     Returns: whether communication was successfully established
     */
-    bool begin();
+    bool begin() override;
     /*
     Base transmit method, called to start a new transmission
     - message : the message to be transmitted
     - len : the length of the message
     Returns: whether a transmission was successfully started
     */
-    bool tx(const uint8_t *message, int len = -1);
+    bool tx(const uint8_t *message, int len = -1) override;
     /*
     Base receive method, called to set the radio to receive mode
     Returns: whether the radio was successfully placed into receive mode
     */
-    bool rx();
+    bool rx() override;
     /*
     Send a message using the data in ```data```
     - data : the ```Data``` object containing the message to be sent
     Returns: whether the transmission was successfully started
     */
-    bool send(Data &data);
+    bool send(Data &data) override;
     /*
     Receive a message and place it in ```data```, should only be called after avail() returns true
     - data : the ```Data``` object to place the message data in
     Returns: whether a message was successfully received
     */
-    bool receive(Data &data);
+    bool receive(Data &data) override;
     /*
     Get recevied signal strength in dBm
     Returns: the recevied signal strength in dBm
     */
-    int RSSI();
+    int RSSI() override;
+
+    // radio class functionality extensions
+    /*
+    Wraps setting a user specified WDS config before normal begin(), rather than using the default
+    - config : the configuration array (from header file)
+    - length : the length of the configuration array
+    */
+    bool begin(const uint8_t *config, uint32_t length);
+    /*
+    Similar to tx(), but starts transmitting without all the bytes available yet
+    Remaining bytes need to be made available via writeTXBuf()
+    - data : the data to start transmitting with
+    - len : the length of the data
+    - totalLen : the total length of data to be transmitted (including bytes yet to be made available)
+    Returns: whether a transmission was successfully started
+    */
+    bool startTX(const uint8_t *data, uint16_t len, uint16_t totalLen);
+    /*
+    Used in conjunction with startTX() to make remaining bytes in the transmission available
+    - data : the data to add to the transmission
+    - len : the length of the data
+    Returns: the length of data successfully added
+    */
+    uint16_t writeTXBuf(const uint8_t *data, uint16_t len);
+    /*
+    Similar to writeTXBuf(), can be used to read from the RX buffer before the full message has been received
+    - data : the array to read data into
+    - len : the length of data to read
+    Returns: the length of data successfully added
+    */
+    uint16_t readRXBuf(uint8_t *data, uint16_t len);
 
     // tx/rx helper functions
     /*
@@ -162,6 +238,13 @@ public:
     */
     void setPower(uint8_t pwr);
     /*
+    Sets the packet structure and processing configuration of the radio
+    - mod : sets the modulation used for the packet (needed to switch between 2 and 4 level FSK)
+    - preambleLength : sets the length of the transmitted preamble
+    - preambleThreshold : sets the required preamble length received without error
+    */
+    void setPacketConfig(Si4463Mod mod, uint8_t preambleLength, uint8_t preambleThresh);
+    /*
     Sets the behavior of configurable radio pins
     - gpio0Mode : sets the behavior of GPIO 0
     - gpio1Mode : sets the behavior of GPIO 1
@@ -179,6 +262,16 @@ public:
     - regDMode : sets the value stored in FRR D, does not modify the register if not passed
     */
     void setFRRs(Si4463FRR regAMode, Si4463FRR regBMode = FRR_NO_CHANGE, Si4463FRR regCMode = FRR_NO_CHANGE, Si4463FRR regDMode = FRR_NO_CHANGE);
+    /*
+    Sets the amount of empty space needed in the FIFO for TX_FIFO_EMPTY to go high
+    - size : the amount of empty space in bytes (max 64)
+    */
+    void setTXThreshold(uint8_t size);
+    /*
+    Sets the numbers of bytes needed in the FIFO for RX_FIFO_FULL to go high
+    - size : the number of bytes (max 64)
+    */
+    void setRXThreshold(uint8_t size);
     /*
     Used to enable or disable the radio's Automatic Frequency Control (AFC)
     - enabled : whether the AFC should be enabled
@@ -209,16 +302,18 @@ public:
     Returns: the state of IRQ
     */
     bool irq();
-
-    void shutdown(bool shutdown);
+    /*
+    Controls the power state of the radio and executes the Power On Reset (POR) sequence
+    - shutdown : whether the radio should be shutdown (true) or powered up (false)
+    Returns: whether the radio was successfully put into the desired state
+    */
+    bool shutdown(bool shutdown);
     /*
     Used to read the state of only one FRR
     - index : the FRR to read from
     Returns: the state of the FRR specified by ```index```
     */
     int readFRR(int index);
-
-    void setRegisters();
 
     // high level functions to use various radio commands
     /*
@@ -251,7 +346,12 @@ public:
     - data : the variable to be updated with the property values
     */
     void getProperty(Si4463Group group, const uint8_t num, Si4463Property start, uint8_t *data);
-
+    /*
+    Sets properties by reading sequentially from an array containing the entire command sequence.
+    Unused helper overload to set properties from Si446X library.
+    - data : the command sequence
+    - size : the length of ```data```
+    */
     void setProperty(uint8_t *data, uint8_t size);
     /*
     Reads all the FRRs
@@ -263,16 +363,26 @@ public:
     Completes the power on sequence for the radio, must be called to use the radio after exiting from a shutdown state
     */
     void powerOn();
+    /*
+    Completes the Image Rejection Calibration sequence, currently non-functional
+    */
+    void performIRCAL();
+
     // lower level functions to directly send radio commands
     /*
     Blocks until the Clear to Send (CTS) byte is received
     */
-    void waitCTS();
+    void waitCTS(uint32_t timeout = Si4463::CTS_TIMEOUT);
     /*
-    Checks for the Clear to Send (CTS) byte
+    Checks for the Clear to Send (CTS) byte using SPI
     Returns: whether the CTS byte was sent
     */
     bool checkCTS();
+    /*
+    Checks for the Clear to Send (CTS) byte using GPIO, returns false if CTS pin is not set
+    Returns: whether the CTS byte was sent
+    */
+    bool CTS();
     /*
     Send a command to the radio, used when the command has arguments and a response besides CTS
     - cmd : the command to send
@@ -296,11 +406,23 @@ public:
     - argvCmd : the values of the command arguments
     */
     void sendCommandC(Si4463Cmd cmd, uint8_t argcCmd, uint8_t *argvCmd);
-
-    SPIClass *spi;
-    uint8_t _cs;
+    /*
+    Set a user specified WDS config, rather than using the default
+    - config : the configuration array (from header file)
+    - length : the length of the configuration array
+    */
+    void setRadioConfig(const uint8_t *config, uint32_t length);
+    /*
+    Checks whether to apply the default settings or user specified settings and applies them
+    - mod : sets the modulation type
+    - dataRate : sets the symbol rate
+    - freq : sets the frequency (Hz)
+    */
+    void applyRadioConfig();
 
 private:
+    SPIClass *spi;
+    uint8_t _cs;
     // spi interface
     uint8_t _sdn;
     uint8_t _irq;
@@ -310,6 +432,23 @@ private:
     uint8_t _gp1;
     uint8_t _gp2;
     uint8_t _gp3;
+
+    // other pins (these will be set to one of the above gpio pins)
+    int _cts = -1;
+
+    // WDS radio config variables
+    // array to hold WDS config
+    uint8_t *WDS_CONFIG = nullptr;
+    // length of WDS_CONFIG
+    uint32_t configLen = 0;
+
+    // other config that can be private
+    // timer for byteDelay
+    uint32_t timer = millis();
+    bool TXEmptyFlag = false;
+    bool RXFullFlag = false;
+    // force use of SPI CTS until GPIO is setup
+    bool useSPICTS = true;
 
     // abstractions of low level SPI operations
     /*
@@ -375,6 +514,10 @@ private:
     - MSB : whether the bytes should be written most significant byte first
     */
     static void to_bytes(uint64_t val, uint8_t pos, uint8_t bytePos, uint8_t *arr, bool MSB = true);
+    /*
+    Applies the config from WDS from a header file generated by compileHeaders.py
+    */
+    void applyWDSConfig(bool applyDefault = true);
 };
 
 #endif
