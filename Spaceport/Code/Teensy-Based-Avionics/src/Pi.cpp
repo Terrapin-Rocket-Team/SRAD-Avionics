27386738a46d5676092b14f77a0d8d94ce4ab05e
Drew Brandt <ahbrandt@umd.edu>
2025-05-04 13:56:40 -0400

    flight code

--- Spaceport/Code/Teensy-Based-Avionics/src/Pi.cpp
+++ Spaceport/Code/Teensy-Based-Avionics/src/Pi.cpp
@@ -14,25 +14,34 @@ Pi::Pi(int pinControl, int pinVideo)
     recording = false;
 }
 
-// void Pi::setOn(bool on)
-// {
-//     digitalWrite(this->pinControl, on ? HIGH : LOW);
-//     this->on = on;
-// }
+void Pi::setOn(bool on)
+{
+    if (this->on == on)
+        return;
+    digitalWrite(this->pinControl, LOW);
+    delayMicroseconds(10);
+    digitalWrite(this->pinControl, HIGH);
+    bb.aonoff(mmfs::BUZZER, 50, 5); // Buzz 3 times (100ms on, 100ms off)
+
+    this->on = on;
+}
 
 void Pi::setRecording(bool recording)
 {
-    if(this->recording == recording) return; // If the recording state is the same, do nothing
+    if (this->recording == recording)
+        return; // If the recording state is the same, do nothing
 
-    // bb.aonoff(mmfs::BUZZER, 100, 3); // Buzz 3 times (100ms on, 100ms off)
-    digitalWrite(pinVideo, recording ? LOW : HIGH);
+    bb.aonoff(mmfs::BUZZER, 50, 2); // Buzz 3 times (100ms on, 100ms off)
+    digitalWrite(this->pinVideo, LOW);
+    delayMicroseconds(10);
+    digitalWrite(this->pinVideo, HIGH);
     this->recording = recording;
 }
 
-// bool Pi::isOn()
-// {
-//     return on;
-// }
+bool Pi::isOn()
+{
+    return on;
+}
 
 bool Pi::isRecording()
 {
