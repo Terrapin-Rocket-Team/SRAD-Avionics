dd2a57d0cc8349f7b77d743b3d34a63942a161a1
twill39 <theowill1@icloud.com>
2025-10-02 19:51:52 -0400

    Bitdelay

---
+++ Spaceport/Code/SDRCode/Bitdelay.py
@@ -0,0 +1,19 @@
+from ConvertToBinary import strToBinary
+
+# takes a string, changes it to binary and encodes it as described here 
+# under differential coding: https://pysdr.org/content/digital_modulation.html
+def bitDelayed(self, strToChange):
+    final_binary_str, final_binary_arr = strToBinary(strToChange)
+    other_bit = 1
+    bit_delayed_str = ""
+
+    for i in range(len(final_binary_str)):
+        next_bit = int(final_binary_str[i] ^ other_bit)
+        bit_delayed_str += next_bit
+        other_bit = next_bit
+
+    return bit_delayed_str
+
+
+
+
