ed29a1e56314745ffc9fd3df1af9c929ff56b068
IsarUslu <110565260+IsarUslu@users.noreply.github.com>
2025-10-03 18:03:05 -0400

    updated tx again

--- Spaceport/Code/SDRCode/Bitdelay.py
+++ Spaceport/Code/SDRCode/Bitdelay.py
@@ -1,19 +1,18 @@
-from ConvertToBinary import strToBinary
+from ConvertToBinary import ConvertToBinary
 
 # takes a string, changes it to binary and encodes it as described here 
 # under differential coding: https://pysdr.org/content/digital_modulation.html
-def bitDelayed(self, strToChange):
-    final_binary_str, final_binary_arr = strToBinary(strToChange)
+def bitDelayed(strToChange):
+    final_binary_str, final_binary_arr = ConvertToBinary().strToBinary(strToChange)
     other_bit = 1
     bit_delayed_str = ""
 
     for i in range(len(final_binary_str)):
-        next_bit = int(final_binary_str[i] ^ other_bit)
+        next_bit = int(final_binary_str[i]) ^ other_bit
         bit_delayed_str += next_bit
         other_bit = next_bit
 
     return bit_delayed_str
-
 
 
 
--- Spaceport/Code/SDRCode/Tx.py
+++ Spaceport/Code/SDRCode/Tx.py
@@ -1,6 +1,8 @@
 import numpy as np
+import matplotlib.pyplot as plt
 #from hackrf import HackRF
 from Bitdelay import bitDelayed
+from ConvertToBinary import ConvertToBinary
 
 # CENTER_FREQ = 1250000000    # Hz
 # L = 24                      # oversampling factor,L=Tb/Ts(Tb=bit period,Ts=sampling period)
@@ -8,16 +10,27 @@ from Bitdelay import bitDelayed
 
 class Tx:
 
-    def __init__(self, data, sampleRate):
-        self.data_str_8b = bitDelayed(data)
-        self.imaginaryForm_np_array = np.array([])
-        self
+    def __init__(self, data):
+        tempObj = ConvertToBinary()
+        self.data_str_8b = tempObj.strToBinary(data)
+        self.imaginaryForm_np_array = []
 
-    def modulate(self, sampleRate):
+    def modulate(self, sampleRate = 10**6):
+
+        phaseShift = 0
+
         for bit in self.data_str_8b:
             if bit == 0:
-                pass
+                phaseShift = 0
             elif bit == 1:
-                pass
+                phaseShift = np.pi
+            
+            print(np.exp(
+                1j *
+                (2*np.pi) *
+                (433 * (10**6)) * 
+                ((len(self.data_str_8b)) / sampleRate) + phaseShift
+            ))
 
-test = Tx("asdfsdfds")
+test = Tx("0101")
+test.modulate()
