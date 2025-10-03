e932ae77a0fad6b9a63c428cf2f8af87e7b3ffed
KC3YKX <louie.m.shapiro@gmail.com>
2025-10-03 15:09:13 -0400

    Create Tx.py

---
+++ Spaceport/Code/SDRCode/Tx.py
@@ -0,0 +1,13 @@
+import numpy as np
+from hackrf import HackRF
+
+# CENTER_FREQ = 1250000000    # Hz
+# L = 24                      # oversampling factor,L=Tb/Ts(Tb=bit period,Ts=sampling period)
+# SAMPLE_RATE = # bit / sec
+
+def modulate(tx_buff):
+
+
+
+if __name__ == "__main__":
+    main()  
