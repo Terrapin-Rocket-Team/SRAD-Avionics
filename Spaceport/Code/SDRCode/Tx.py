1c1c12e146633988a4ee742c75fa3e15b9275188
IsarUslu <110565260+IsarUslu@users.noreply.github.com>
2025-10-03 15:28:42 -0400

    added tx class to utilize ctb

--- Spaceport/Code/SDRCode/Tx.py
+++ Spaceport/Code/SDRCode/Tx.py
@@ -1,13 +1,22 @@
 import numpy as np
-from hackrf import HackRF
+#from hackrf import HackRF
+from ConvertToBinary import ConvertToBinary
 
 # CENTER_FREQ = 1250000000    # Hz
 # L = 24                      # oversampling factor,L=Tb/Ts(Tb=bit period,Ts=sampling period)
 # SAMPLE_RATE = # bit / sec
 
-def modulate(tx_buff):
+class Tx:
 
+    def __init__(self, data):
+        self.data_str_8b, self.data_arr_8b = ConvertToBinary().strToBinary(data)
+        self.imaginaryForm = np.array([])
 
+    def modulate(self, sampleRate):
+        for bit in self.data_str_8b:
+            if bit == 0:
+                pass
+            elif bit == 1:
+                pass
 
-if __name__ == "__main__":
-    main()  
+test = Tx("asdfsdfds")
