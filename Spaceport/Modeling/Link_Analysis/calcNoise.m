7a8d970d4b3d94d3e56b0fa837de13cc7fe72d92
KC3YKX <louie.m.shapiro@gmail.com>
2025-10-19 20:31:04 -0400

    add systemNoiseTemp to calcNoise.m

--- Spaceport/Modeling/Link_Analysis/calcNoise.m
+++ Spaceport/Modeling/Link_Analysis/calcNoise.m
@@ -4,6 +4,9 @@ function noise = calcNoise(symbolRate, order, modIndex, noiseFig)
     T0 = 290; % K
     systemNoisePower = physconst("Boltzmann") * T0 * occupiedBW(symbolRate, order, modIndex);
 
+    systemNoiseTemp = (systemNoisePower / occupiedBW(symbolRate, order, modIndex)) * (1 / physconst("Boltzmann"));
+
     % add noise of everything else
-    noise = 10*log10(systemNoisePower*1000) + noiseFig;
+    noise = 10*log10(systemNoisePower*1000) + noiseFig + systemNoiseTemp;
+
 end
