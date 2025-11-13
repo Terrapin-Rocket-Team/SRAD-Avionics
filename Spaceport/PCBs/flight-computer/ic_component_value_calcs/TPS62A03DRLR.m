349bc3fbfd657645a8e765241c256592143f2072
KC3YKX <louie.m.shapiro@gmail.com>
2025-11-13 12:26:25 -0500

    power stuff

--- Spaceport/PCBs/flight-computer/ic_component_value_calcs/TPS62A03DRLR.m
+++ Spaceport/PCBs/flight-computer/ic_component_value_calcs/TPS62A03DRLR.m
@@ -1,8 +1,12 @@
 %%
 clc; close all; clear
 
-V_OUT = 4.5;
-R2 = 200e3;
+V_OUT = 4.5; % 3.3V + V_DROPOUT (= 1.2V)
+R2 = 100e3;
 V_FB = .6;
 
-R1 = R2 * ((V_OUT / V_FB) - 1))
+R1 = R2 * ((V_OUT / V_FB) - 1)
+
+%% R1 Actual
+% R1 = 649e3;
+% V_OUT = V_FB * ((R1 + R2) / R2)
