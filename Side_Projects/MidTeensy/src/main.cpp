0b7b8aac0e24380ba58d4f735ae915e3a54a06ba
jhauerst <jhauerst@terpmail.umd.edu>
2026-03-21 01:14:03 -0400

    fix mid teensy
    its now slightly less mid teensy

--- Side_Projects/MidTeensy/src/main.cpp
+++ Side_Projects/MidTeensy/src/main.cpp
@@ -5,167 +5,146 @@
 namespace
 {
 
-constexpr uint32_t kUsbBaud = 115200;
-constexpr uint32_t kPacketBaud = 115200;
-constexpr uint32_t kPacketTimeoutMs = 100;
-constexpr size_t kHeaderSize = 2;
-constexpr size_t kMaxPayloadSize = 255;
-constexpr size_t kMaxPacketSize = kHeaderSize + kMaxPayloadSize;
+    constexpr uint32_t kUsbBaud = 115200;
+    constexpr uint32_t kPacketBaud = 115200;
+    constexpr uint32_t kPacketTimeoutMs = 100;
+    constexpr size_t kMaxPayloadSize = 255;
 
-HardwareSerial &kRadioPort = Serial1;
-HardwareSerial &kAvionicsPort = Serial3;
-HardwareSerial &kAirbrakePort = Serial5;
+    HardwareSerial &kRadioPort = Serial1;
+    HardwareSerial &kAvionicsPort = Serial5;
+    HardwareSerial &kAirbrakePort = Serial3;
 
-enum class RxState : uint8_t
-{
-    WaitingForType,
-    WaitingForLength,
-    WaitingForPayload,
-};
-
-struct PacketParser
-{
-    const char *name = "";
-    HardwareSerial *input = nullptr;
-    RxState state = RxState::WaitingForType;
-    uint8_t packet[kMaxPacketSize] = {};
-    uint8_t expectedPayloadLength = 0;
-    uint8_t payloadBytesRead = 0;
-    elapsedMillis lastByteAge = 0;
-    uint32_t forwardedPackets = 0;
-    uint32_t droppedPackets = 0;
-};
-
-PacketParser avionicsParser = {
-    "AVIONICS",
-    &kAvionicsPort,
-};
-
-PacketParser airbrakeParser = {
-    "AIRBRAKE",
-    &kAirbrakePort,
-};
-
-elapsedMillis statusAge;
-
-void resetParser(PacketParser &parser)
-{
-    parser.state = RxState::WaitingForType;
-    parser.expectedPayloadLength = 0;
-    parser.payloadBytesRead = 0;
-    parser.lastByteAge = 0;
-}
-
-void printHexBytes(const uint8_t *bytes, size_t length)
-{
-    for (size_t i = 0; i < length; ++i)
+    enum class RxState : uint8_t
     {
-        Serial.printf("%02X", bytes[i]);
-        if ((i + 1U) < length)
-        {
-            Serial.print(' ');
-        }
-    }
-}
+        WaitingForInput,
+        WaitingForFinish,
+    };
 
-void forwardPacket(PacketParser &parser, size_t packetSize)
-{
-    kRadioPort.write(parser.packet, packetSize);
-    ++parser.forwardedPackets;
-
-    Serial.printf("%s -> RADIO len=%u raw=",
-                  parser.name,
-                  static_cast<unsigned>(packetSize));
-    printHexBytes(parser.packet, packetSize);
-    Serial.println();
-}
-
-void dropPacket(PacketParser &parser, const char *reason)
-{
-    ++parser.droppedPackets;
-    Serial.printf("%s drop: %s\n", parser.name, reason);
-    resetParser(parser);
-}
-
-void beginPacket(PacketParser &parser, uint8_t type)
-{
-    parser.packet[0] = type;
-    parser.state = RxState::WaitingForLength;
-    parser.lastByteAge = 0;
-}
-
-void consumeByte(PacketParser &parser, uint8_t byte)
-{
-    parser.lastByteAge = 0;
-
-    switch (parser.state)
+    struct PacketParser
     {
-    case RxState::WaitingForType:
-        beginPacket(parser, byte);
-        return;
+        const char *name = "";
+        HardwareSerial *input = nullptr;
+        RxState state = RxState::WaitingForInput;
+        uint8_t packet[kMaxPayloadSize] = {};
+        uint8_t payloadBytesRead = 0;
+        elapsedMillis lastByteAge = 0;
+        uint32_t forwardedPackets = 0;
+        uint32_t droppedPackets = 0;
+    };
 
-    case RxState::WaitingForLength:
-        parser.packet[1] = byte;
-        parser.expectedPayloadLength = byte;
+    PacketParser avionicsParser = {
+        "AVIONICS",
+        &kAvionicsPort,
+    };
+
+    PacketParser airbrakeParser = {
+        "AIRBRAKE",
+        &kAirbrakePort,
+    };
+
+    elapsedMillis statusAge;
+
+    void resetParser(PacketParser &parser)
+    {
+        parser.state = RxState::WaitingForInput;
         parser.payloadBytesRead = 0;
+        parser.lastByteAge = 0;
+    }
 
-        if (byte > kMaxPayloadSize)
+    void printHexBytes(const uint8_t *bytes, size_t length)
+    {
+        for (size_t i = 0; i < length; ++i)
         {
-            dropPacket(parser, "payload too long");
+            Serial.printf("%02X", bytes[i]);
+            if ((i + 1U) < length)
+            {
+                Serial.print(' ');
+            }
+        }
+    }
+
+    void forwardPacket(PacketParser &parser, size_t packetSize)
+    {
+        kRadioPort.write(parser.packet, packetSize);
+        ++parser.forwardedPackets;
+
+        Serial.printf("%s -> RADIO len=%u raw=",
+                      parser.name,
+                      static_cast<unsigned>(packetSize));
+        Serial.write(parser.packet, packetSize);
+        Serial.println();
+    }
+
+    void dropPacket(PacketParser &parser, const char *reason)
+    {
+        ++parser.droppedPackets;
+        Serial.printf("%s drop: %s\n", parser.name, reason);
+        resetParser(parser);
+    }
+
+    void beginPacket(PacketParser &parser, uint8_t byte)
+    {
+        parser.packet[0] = byte;
+        parser.payloadBytesRead = 1;
+        parser.state = RxState::WaitingForFinish;
+        parser.lastByteAge = 0;
+    }
+
+    void consumeByte(PacketParser &parser, uint8_t byte)
+    {
+        parser.lastByteAge = 0;
+
+        switch (parser.state)
+        {
+        case RxState::WaitingForInput:
+            beginPacket(parser, byte);
+            return;
+        case RxState::WaitingForFinish:
+            parser.packet[parser.payloadBytesRead] = byte;
+            ++parser.payloadBytesRead;
+
+            if (byte == '\n')
+            {
+                forwardPacket(parser, parser.payloadBytesRead);
+                resetParser(parser);
+            }
+            if (parser.payloadBytesRead > kMaxPayloadSize)
+            {
+                dropPacket(parser, "payload too long");
+                return;
+            }
             return;
         }
+    }
 
-        if (byte == 0)
+    void serviceInput(PacketParser &parser)
+    {
+        while (parser.input->available() > 0)
         {
-            forwardPacket(parser, kHeaderSize);
-            resetParser(parser);
-            return;
+            const int incoming = parser.input->read();
+            if (incoming < 0)
+            {
+                continue;
+            }
+
+            consumeByte(parser, static_cast<uint8_t>(incoming));
         }
 
-        parser.state = RxState::WaitingForPayload;
-        return;
-
-    case RxState::WaitingForPayload:
-        parser.packet[kHeaderSize + parser.payloadBytesRead] = byte;
-        ++parser.payloadBytesRead;
-
-        if (parser.payloadBytesRead >= parser.expectedPayloadLength)
+        if (parser.state != RxState::WaitingForInput &&
+            parser.lastByteAge > kPacketTimeoutMs)
         {
-            forwardPacket(parser, kHeaderSize + parser.expectedPayloadLength);
-            resetParser(parser);
+            dropPacket(parser, "timeout while receiving packet");
         }
-        return;
     }
-}
 
-void serviceInput(PacketParser &parser)
-{
-    while (parser.input->available() > 0)
+    void waitForUsbSerial()
     {
-        const int incoming = parser.input->read();
-        if (incoming < 0)
+        const uint32_t start = millis();
+        while (!Serial && (millis() - start) < 3000)
         {
-            continue;
+            delay(10);
         }
-
-        consumeByte(parser, static_cast<uint8_t>(incoming));
     }
-
-    if (parser.state != RxState::WaitingForType &&
-        parser.lastByteAge > kPacketTimeoutMs)
-    {
-        dropPacket(parser, "timeout while receiving packet");
-    }
-}
-
-void waitForUsbSerial()
-{
-    const uint32_t start = millis();
-    while (!Serial && (millis() - start) < 3000)
-    {
-        delay(10);
-    }
-}
 
 } // namespace
 
